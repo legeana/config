cfd7607e0e7a82593b4da6641ec7d057c3154901
Lisa White <lisa.rsfp+dev@gmail.com>
2015-09-27 21:52:00 +0400

    Added fish aliases.

---
+++ .config/fish/aliases/misc.fish
@@ -0,0 +1,39 @@
+function assh
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $argv
+end
+
+function df
+    command df -h $argv
+end
+
+function dirf
+    find . -type d | sed -e "s/[^-][^\/]*\//  |/g" -e "s/|\([^ ]\)/|-\1/" $argv
+end
+
+function du
+    command du -sh $argv
+end
+
+function emacs
+    command emacs -nw
+end
+
+function fbi
+    fbi -a $argv
+end
+
+function feh
+    feh --scale-down $argv
+end
+
+function rplayer
+    mplayer -loop 0 -shuffle $argv
+end
+
+function psc
+    ps xawf -eo pid,user,cgroup,args $argv
+end
+
+function rabbitmqctl
+    sudo -u rabbitmq rabbitmqctl $argv
+end
