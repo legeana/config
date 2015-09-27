c6042a01d14820e37483d73d1d759b319b0144b3
Lisa White <lisa.rsfp+dev@gmail.com>
2015-09-27 22:56:21 +0400

    Added fish colors.

---
+++ .config/fish/config.d/colors.fish
@@ -0,0 +1,21 @@
+set -U fish_color_autosuggestion 969896
+set -U fish_color_command c397d8
+set -U fish_color_comment e7c547
+set -U fish_color_cwd green
+set -U fish_color_cwd_root
+set -U fish_color_end c397d8
+set -U fish_color_error d54e53
+set -U fish_color_escape cyan
+set -U fish_color_history_current cyan
+set -U fish_color_host '-o'  'cyan'
+set -U fish_color_match cyan
+set -U fish_color_normal normal
+set -U fish_color_operator cyan
+set -U fish_color_param 7aa6da
+set -U fish_color_quote b9ca4a
+set -U fish_color_redirection 70c0b1
+set -U fish_color_search_match --background=purple
+set -U fish_color_selection --background=purple
+set -U fish_color_status red
+set -U fish_color_user '-o'  'green'
+set -U fish_color_valid_path --underline
