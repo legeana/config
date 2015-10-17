d12564021815a4cd51325c50625369e7095ae142
Lisa White <lisa.rsfp+dev@gmail.com>
2015-10-18 00:26:33 +0400

    Fixed cross-platform sed usage in fish configuration.

--- .config/fish/config.d/10_profile.fish
+++ .config/fish/config.d/10_profile.fish
@@ -2,4 +2,4 @@ env -i HOME=$HOME sh -l -c 'source /etc/profile ; printenv' | \
     sed -e '/^PWD=/d;/^SHLVL=/d;/^_/d;/PATH/s/:/ /g;s/=/ /;s/^/set -x /' | \
     source
 
-manpath | sed -r 's|:| |g;s|^|set -gx MANPATH |' | source
+manpath | sed 's|:| |g;s|^|set -gx MANPATH |' | source
