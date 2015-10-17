adbf400d5e2c095b6875470b8a1e7e7ac16ea76b
Lisa White <lisa.rsfp+dev@gmail.com>
2015-10-18 00:14:57 +0400

    Fixed fish add_to_list().

--- .config/fish/functions/add_to_list.fish
+++ .config/fish/functions/add_to_list.fish
@@ -1,7 +1,9 @@
 function add_to_list
-    for arg in $argv[-1..2]
-        if not contains $arg $$argv[1]
-            set $argv[1] $arg $$argv[1]
+    if [ (count $argv) -gt 1 ]
+        for arg in $argv[-1..2]
+            if not contains $arg $$argv[1]
+                set $argv[1] $arg $$argv[1]
+            end
         end
     end
 end
