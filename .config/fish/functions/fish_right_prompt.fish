21beb16d9c75a49b8385606bfa3b6ce892d42e3f
Lisa White <lisa.rsfp+dev@gmail.com>
2015-10-21 18:29:48 +0400

    Added command duration to fish right prompt.

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,9 @@
 function fish_right_prompt -d "Write out the right prompt"
     set -l external_status $status
+    set -l external_cmd_duration $CMD_DURATION
+    if [ (count $external_cmd_duration) -eq 0 ]
+        set external_cmd_duration 0
+    end
     switch "$external_status"
     case 0
         set_color green
@@ -7,9 +11,9 @@ function fish_right_prompt -d "Write out the right prompt"
         set_color red
     end
     echo -s -n $external_status ' '
+    set_color magenta
+    echo -s -n {$external_cmd_duration}ms ' '
     set_color cyan
     date "+%H:%M %a %d "
     set_color normal
 end
-
-#export RPROMPT='%(?.%F{green}.%F{red})%?%f %F{cyan}%U%T%u %U%w%u%f'
