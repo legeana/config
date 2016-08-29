2136a51a042e37339a823591f3402f6dc8c4b859
Lisa White <lisa.rsfp+dev@gmail.com>
2016-08-29 17:46:19 +0400

    Fixed fish git prompt.

--- .config/fish/config.d/90_git_prompt.fish
+++ .config/fish/config.d/90_git_prompt.fish
@@ -7,9 +7,23 @@ set -U __fish_git_prompt_color_branch yellow
 set -U __fish_git_prompt_color_upstream_ahead green
 set -U __fish_git_prompt_color_upstream_behind red
 
-set -U __fish_git_prompt_char_dirtystate '⚡'
-set -U __fish_git_prompt_char_stagedstate '→'
-set -U __fish_git_prompt_char_untrackedfiles '☡'
-set -U __fish_git_prompt_char_stashstate '↩'
-set -U __fish_git_prompt_char_upstream_ahead '↑'
-set -U __fish_git_prompt_char_upstream_behind '↓'
+# Clean previously incorrectly used universal variables.
+function __clean_fish_git_prompt
+    set -e $argv[1]
+    set -g $argv[1] $argv[2]
+end
+
+__clean_fish_git_prompt __fish_git_prompt_char_cleanstate '✔'
+__clean_fish_git_prompt __fish_git_prompt_char_dirtystate '*' '✚'
+__clean_fish_git_prompt __fish_git_prompt_char_invalidstate '#' '✖'
+__clean_fish_git_prompt __fish_git_prompt_char_stagedstate '+' '●'
+__clean_fish_git_prompt __fish_git_prompt_char_stashstate '$'
+__clean_fish_git_prompt __fish_git_prompt_char_stateseparator ' ' '|'
+__clean_fish_git_prompt __fish_git_prompt_char_untrackedfiles '%' '…'
+__clean_fish_git_prompt __fish_git_prompt_char_upstream_ahead '>' '↑'
+__clean_fish_git_prompt __fish_git_prompt_char_upstream_behind '<' '↓'
+__clean_fish_git_prompt __fish_git_prompt_char_upstream_diverged '<>'
+__clean_fish_git_prompt __fish_git_prompt_char_upstream_equal '='
+__clean_fish_git_prompt __fish_git_prompt_char_upstream_prefix ''
+
+functions --erase __clean_fish_git_prompt
