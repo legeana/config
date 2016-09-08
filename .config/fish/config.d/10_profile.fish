9c930d278fefc13fdcf397ee814e10e9acf66a10
Lisa White <lisa.rsfp+dev@gmail.com>
2016-09-08 18:28:03 +0100

    Escape quote strings and escape \ and " in fish profile fetcher.

--- .config/fish/config.d/10_profile.fish
+++ .config/fish/config.d/10_profile.fish
@@ -1,7 +1,14 @@
 set -e MANPATH
 
 env -i HOME=$HOME sh -l -c 'source /etc/profile ; printenv' | \
-    sed -e '/^PWD=/d;/^SHLVL=/d;/^_/d;/PATH/s/:/ /g;s/=/ /;s/^/set -gx /' | \
+    sed -e '/^PWD=/d;
+            /^SHLVL=/d;
+            /^_/d;
+            /PATH/s/:/ /g;
+            s/"/\\"/g;
+            s/\\\\/\\\\/g;
+            s/\(^[^=]*\)=\(.*\)$/\1 "\2"/;
+            s/^/set -gx /' | \
     source
 
 manpath | sed 's|:| |g;s|^|set -gx MANPATH |' | source
