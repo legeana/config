54fd7943720011043a921623a7a212df39029388
Lisa White <lisa.rsfp+dev@gmail.com>
2017-05-27 17:35:07 -0400

    Fixed fish plugin loading behavior for empty plugin directory.

--- .config/fish/functions/load_plugins.fish
+++ .config/fish/functions/load_plugins.fish
@@ -1,13 +1,7 @@
 function load_plugins
-    for plugin in $argv
-        if test -d $plugin/functions
-            add_to_function_path $plugin/functions
-        end
-        if test -d $plugin/completions
-            add_to_complete_path $plugin/completions
-        end
-        if test -d $plugin/config.d
-            load_d $plugin/config.d
+    for plugin_dir in $argv
+        for plugin in $plugin_dir/*
+            load_plugin $plugin
         end
     end
 end
