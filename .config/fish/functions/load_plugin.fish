0efe820dbbc3395f8ad5ffc42866762401f6adc7
Lisa White <lisa.rsfp+dev@gmail.com>
2017-05-28 23:25:42 +0100

    Added fish load_plugin compatibility support for conf.d and bare repositories.

--- .config/fish/functions/load_plugin.fish
+++ .config/fish/functions/load_plugin.fish
@@ -6,8 +6,16 @@ function load_plugin
         if test -d $plugin/completions
             add_to_complete_path $plugin/completions
         end
+        if test -d $plugin/conf.d
+            load_d $plugin/conf.d
+        end
         if test -d $plugin/config.d
             load_d $plugin/config.d
+        end
+        if not test -d $plugin/functions
+           and not test -d $plugin/conf.d
+           and not test -d $plugin/config.d
+            add_to_function_path $plugin
         end
     end
 end
