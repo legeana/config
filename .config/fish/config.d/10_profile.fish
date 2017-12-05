681d5bf3c94bc1cf42b85c8bf1c6dba645f90bb2
Lisa White <lisa.rsfp+dev@gmail.com>
2017-12-05 03:51:24 +0000

    Fixed fish /etc/profile parsing: use '.' instead of 'source' in sh.

--- .config/fish/config.d/10_profile.fish
+++ .config/fish/config.d/10_profile.fish
@@ -1,6 +1,6 @@
 set -e MANPATH
 
-env -i HOME=$HOME sh -l -c 'source /etc/profile ; printenv' | \
+env -i HOME=$HOME sh -l -c '. /etc/profile ; printenv' | \
     sed -e '/^PWD=/d;
             /^SHLVL=/d;
             /^_/d;
