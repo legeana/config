e4403d7a96c2d19ffcac182c358870b325f4080f
Lisa White <lisa.rsfp+dev@gmail.com>
2019-12-11 07:43:17 -0800

    Fail is_local_filesystem on stat error

--- base/.config/fish/functions/is_local_filesystem.fish
+++ base/.config/fish/functions/is_local_filesystem.fish
@@ -3,6 +3,10 @@ function is_local_filesystem -a path
         set path $PWD
     end
     set -l fstype (stat --file-system  --format='%T' $path)
+    if test (count $fstype) = 0
+        # this is not a filesystem
+        return 1
+    end
     switch $fstype
         case 'fuseblk'
             return 1
