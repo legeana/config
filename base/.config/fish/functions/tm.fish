ceefbf4b9af238b7d587f5ebf3a6b217cc8c147f
Lisa White <lisa.rsfp+dev@gmail.com>
2020-03-26 08:03:36 -0700

    Forward SSH_AUTH_SOCK and FWD_SSH_AUTH_SOCK into tmux

--- base/.config/fish/functions/tm.fish
+++ base/.config/fish/functions/tm.fish
@@ -1,9 +1,34 @@
-function tm
-    set -l session default
-    if set -q argv[1]
-        set session $argv[1]
+function tm -a session
+    if test -z "$session"
+        set session default
     end
-    tmux attach -t $session ; or tmux new-session -s $session -c $HOME
+    set env_overrides
+    _tm_forward $session
+    env $env_overrides tmux attach -t $session
+    or env $env_overrides tmux new-session -s $session -c $HOME
 end
 
 complete --command=tm --no-files --arguments='(tmux list-sessions -F "#{session_name}")'
+
+# The functions below operate under the assumption that tmux will not be used
+# by more than one user simultaneously. The last connection wins.
+
+# TODO GPG_TTY forwarding
+
+set -g _TM_TMP "$HOME/.tmux-tmp"
+
+function _tm_forward_socket --no-scope-shadowing -a session -a env
+    mkdir -p -m 700 "$_TM_TMP"
+    set path "$_TM_TMP/$USER.$session.$env"
+    if ! set -q $env || ! test -S $$env
+        rm -f $path
+    else
+        ln -snf -- $$env $path
+    end
+    set --append env_overrides "$env=$path"
+end
+
+function _tm_forward --no-scope-shadowing -a session
+    _tm_forward_socket $session SSH_AUTH_SOCK
+    _tm_forward_socket $session FWD_SSH_AUTH_SOCK
+end
