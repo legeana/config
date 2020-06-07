bcd9f92abc135df9e977f54417ed1e1c66b05cdb
Lisa White <lisa.rsfp+dev@gmail.com>
2020-06-07 19:34:16 +0100

    Fix confgen.py style

--- base/bin/confgen.py
+++ base/bin/confgen.py
@@ -10,33 +10,37 @@ from jinja2 import BaseLoader, Environment, PackageLoader
 
 class ConfGenLoader(BaseLoader):
 
-    def __init__(self, path):
-        self._path = path
+  def __init__(self, path):
+    self._path = path
 
-    def get_source(self, environment, template):
-        path = join(self._path, template)
-        if not exists(path):
-            raise TemplateNotFound(template)
-        mtime = getmtime(path)
-        with open(path) as f:
-            source = f.read()
-        return source, path, lambda: mtime == getmtime(path)
+  def get_source(self, environment, template):
+    path = join(self._path, template)
+    if not exists(path):
+      raise TemplateNotFound(template)
+    mtime = getmtime(path)
+    with open(path) as f:
+      source = f.read()
+    return source, path, lambda: mtime == getmtime(path)
+
+
+def main():
+  cwd = os.getcwd()
+  templates = join(cwd, '.confgen')
+  generate = join(cwd, '.confgen', 'generate.json')
+  if not exists(templates):
+    print('Create "{}" directory in order to use ConfGen'.format(templates))
+    sys.exit(2)
+  if not exists(generate):
+    print('Create "{}" file in order to use ConfGen'.format(generate))
+    sys.exit(3)
+  env = Environment(loader=ConfGenLoader(templates))
+  with open(generate) as gen:
+    generate = json.load(gen)
+  for i in generate:
+    templ = env.get_template(i)
+    with open(i, "w") as f:
+      print(templ.render(), file=f)
 
 
 if __name__=='__main__':
-    cwd = os.getcwd()
-    templates = join(cwd, '.confgen')
-    generate = join(cwd, '.confgen', 'generate.json')
-    if not exists(templates):
-        print('Create "{}" directory in order to use ConfGen'.format(templates))
-        sys.exit(2)
-    if not exists(generate):
-        print('Create "{}" file in order to use ConfGen'.format(generate))
-        sys.exit(3)
-    env = Environment(loader=ConfGenLoader(templates))
-    with open(generate) as gen:
-        generate = json.load(gen)
-    for i in generate:
-        templ = env.get_template(i)
-        with open(i, "w") as f:
-            print(templ.render(), file=f)
+  main()
