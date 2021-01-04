14849e269a1af8762f9824d9ff037239bd8403cb
Lisa White <lisa.rsfp+dev@gmail.com>
2021-01-04 16:27:07 +0000

    Add /usr/local/opt/*/{,s}bin to PATH for MacOS

--- base/.config/fish/config.d/20_os_x_homebrew.fish
+++ base/.config/fish/config.d/20_os_x_homebrew.fish
@@ -1,3 +1,4 @@
 set _list /usr/local/opt/*/libexec/gnubin; add_to_path $_list
 add_to_path /usr/local/sbin
 set _list /usr/local/opt/*/libexec/gnuman; add_to_manpath $_list
+set _list /usr/local/opt/*/{,s}bin; add_to_path $_list
