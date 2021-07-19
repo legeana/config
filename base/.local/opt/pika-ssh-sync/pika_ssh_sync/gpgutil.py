269a7ef317afb28d35f05647d0d623ec8d2a48bb
Lisa White <legeana@liri.ie>
2021-07-19 18:40:25 +0100

    Make pika-ssh-sync mypy-compliant
    Add gpgutil.KeyTokens and make its fields non-optional.
    Replace os.getenv('HOME') with pathlib.Path.home().
    Make gpgutils.BaseKey a dataclass.

--- base/.local/opt/pika-ssh-sync/pika_ssh_sync/gpgutil.py
+++ base/.local/opt/pika-ssh-sync/pika_ssh_sync/gpgutil.py
@@ -7,68 +7,85 @@ from typing import List, Optional
 
 @enum.unique
 class Algorithm(enum.Enum):
-  rsa_rsa =  1 # RSA and RSA (default)
-  dsa_elg =  2 # DSA and Elgamal
-  dsa     =  3 # DSA (sign only)
-  rsa_s   =  4 # RSA (sign only)
-  elg     =  5 # Elgamal (encrypt only)
-  rsa_e   =  6 # RSA (encrypt only)
-  dsa_a   =  7 # DSA (set your own capabilities)
-  rsa_a   =  8 # RSA (set your own capabilities)
-  ecc_ecc =  9 # ECC and ECC
-  ecc_s   = 10 # ECC (sign only)
-  ecc_a   = 11 # ECC (set your own capabilities)
-  ecc_e   = 12 # ECC (encrypt only)
-  keygrip = 13 # Existing key
+  UNKNOWN = -1 # not set properly
+  RSA_RSA =  1 # RSA and RSA (default)
+  DSA_ELG =  2 # DSA and Elgamal
+  DSA     =  3 # DSA (sign only)
+  RSA_S   =  4 # RSA (sign only)
+  ELG     =  5 # Elgamal (encrypt only)
+  RSA_E   =  6 # RSA (encrypt only)
+  DSA_A   =  7 # DSA (set your own capabilities)
+  RSA_A   =  8 # RSA (set your own capabilities)
+  ECC_ECC =  9 # ECC and ECC
+  ECC_S   = 10 # ECC (sign only)
+  ECC_A   = 11 # ECC (set your own capabilities)
+  ECC_E   = 12 # ECC (encrypt only)
+  KEYGRIP = 13 # Existing key
 
 
 class Tokens:
 
-  def __init__(self, line):
+  _tokens: List[str]
+
+  def __init__(self, line: str):
     self._tokens = line.split(':')
 
-  def _get(self, index) -> Optional[str]:
+  def get(self, index: int) -> str:
     if 0 < index <= len(self._tokens):
       return self._tokens[index - 1]
-    return None
+    raise IndexError(f'No field with index {index}')
 
-  def field(self) -> Optional[str]:
-    return self._get(1)
+  def type(self) -> str:
+    return self.get(1)
 
-  def validity(self) -> Optional[str]:
-    return self._get(2)
 
-  def length(self) -> Optional[str]:
-    return self._get(3)
+class KeyTokens:
 
-  def algo(self) -> Optional[Algorithm]:
-    a = self._get(4)
+  _tokens: Tokens
+
+  def __init__(self, tokens: Tokens):
+    self._tokens = tokens
+
+  def type(self) -> str:
+    return self._tokens.type()
+
+  def validity(self) -> str:
+    return self._tokens.get(2)
+
+  def length(self) -> str:
+    return self._tokens.get(3)
+
+  def algo(self) -> Algorithm:
+    a = self._tokens.get(4)
     if a:
-      return Algorithm(int(a))
-    return None
+      try:
+        return Algorithm(int(a))
+      except ValueError:
+        pass
+    return Algorithm.UNKNOWN
 
-  def key_id(self) -> Optional[str]:
-    return self._get(5)
+  def key_id(self) -> str:
+    return self._tokens.get(5)
 
-  def creation_date(self) -> Optional[str]:
-    return self._get(6)
+  def creation_date(self) -> str:
+    return self._tokens.get(6)
 
-  def expiration_date(self) -> Optional[str]:
-    return self._get(7)
+  def expiration_date(self) -> str:
+    return self._tokens.get(7)
 
   # 8 Certificate S/N
 
-  def owner_trust(self) -> Optional[str]:
-    return self._get(9)
+  def owner_trust(self) -> str:
+    return self._tokens.get(9)
 
-  def user_id(self) -> Optional[str]:
-    return self._get(10)
+  def user_id(self) -> str:
+    return self._tokens.get(10)
 
-  def signature_class(self) -> Optional[str]:
-    return self._get(11)
+  def signature_class(self) -> str:
+    return self._tokens.get(11)
 
-  def capabilities(self) -> Optional[str]:
-    return self._get(12)
+  def capabilities(self) -> str:
+    return self._tokens.get(12)
 
 
 class DictRepr:
@@ -80,16 +97,17 @@ class DictRepr:
     return pprint.pformat(self._drepr())
 
 
+@dataclasses.dataclass
 class BaseKey(DictRepr):
-  fingerprint = None
-  validity = None
-  length = None
-  algo = None
-  key_id = None
-  creation_date = None
-  expiration_date = None
-  owner_trust = None
-  capabilities = None
+  fingerprint: str = ''
+  validity: str = ''
+  length: str = ''
+  algo: Algorithm = Algorithm.UNKNOWN
+  key_id: str = ''
+  creation_date: str = ''
+  expiration_date: str = ''
+  owner_trust: str = ''
+  capabilities: str = ''
 
   def _drepr(self):
     d = super(BaseKey, self)._drepr()
@@ -129,8 +147,8 @@ class SubKey(BaseKey):
 
 @dataclasses.dataclass
 class Uid(DictRepr):
-  name: Optional[str]
-  validity: Optional[str]
+  name: str
+  validity: str
 
   def _drepr(self):
     d = super(Uid, self)._drepr()
@@ -151,19 +169,19 @@ class Parser:
   def parse(self, line) -> None:
     tokens = Tokens(line)
     if not tokens: return
-    if tokens.field() in {'pub', 'sec'}:
-      self._parse_key(tokens)
-    elif tokens.field() in {'sub', 'ssb'}:
-      self._parse_subkey(tokens)
-    elif tokens.field() in {'uid'}:
-      self._parse_uid(tokens)
-    elif tokens.field() in {'fpr'}:
-      self._parse_fpr(tokens)
+    if tokens.type() in {'pub', 'sec'}:
+      self._parse_key(KeyTokens(tokens))
+    elif tokens.type() in {'sub', 'ssb'}:
+      self._parse_subkey(KeyTokens(tokens))
+    elif tokens.type() in {'uid'}:
+      self._parse_uid(KeyTokens(tokens))
+    elif tokens.type() in {'fpr'}:
+      self._parse_fpr(KeyTokens(tokens))
 
   def keys(self) -> List[Key]:
     return self._keys
 
-  def _fill_key(self, key, tokens: Tokens) -> None:
+  def _fill_key(self, key, tokens: KeyTokens) -> None:
     key.validity = tokens.validity()
     key.length = tokens.length()
     key.key_id = tokens.key_id()
@@ -172,25 +190,25 @@ class Parser:
     key.owner_trust = tokens.owner_trust()
     key.capabilities = tokens.capabilities()
 
-  def _parse_key(self, tokens: Tokens) -> None:
+  def _parse_key(self, tokens: KeyTokens) -> None:
     self._key = Key()
     self._subkey = None
     self._keys.append(self._key)
     self._fill_key(self._key, tokens)
 
-  def _parse_fpr(self, tokens) -> None:
+  def _parse_fpr(self, tokens: KeyTokens) -> None:
     if self._subkey:
       self._subkey.fingerprint = tokens.user_id()
     elif self._key:
       self._key.fingerprint = tokens.user_id()
 
-  def _parse_subkey(self, tokens: Tokens) -> None:
+  def _parse_subkey(self, tokens: KeyTokens) -> None:
     if not self._key: return
     self._subkey = SubKey()
     self._key.subkeys.append(self._subkey)
     self._fill_key(self._subkey, tokens)
 
-  def _parse_uid(self, tokens: Tokens) -> None:
+  def _parse_uid(self, tokens: KeyTokens) -> None:
     self._subkey = None
     uid = Uid(name=tokens.user_id(), validity=tokens.validity())
     assert self._key is not None
