8b10845d5e52d042463d569d42f61b816d1ab585
Lisa White <legeana@liri.ie>
2021-07-19 23:29:15 +0100

    Move pika-ssh-sync configuration into ~/.config/pika-ssh-sync.ini

---
+++ base/.local/opt/pika-ssh-sync/pika_ssh_sync/config.py
@@ -0,0 +1,62 @@
+import configparser
+import dataclasses
+import pathlib
+from typing import List
+
+_LOCATION = pathlib.Path.home() / '.config' / 'pika-ssh-sync.ini'
+
+
+@dataclasses.dataclass
+class Entry:
+  name: str
+
+
+@dataclasses.dataclass
+class KeysAPIEntry(Entry):
+  url: str
+
+
+@dataclasses.dataclass
+class GPGKeyEntry(Entry):
+  fingerprint: str
+
+
+@dataclasses.dataclass
+class GPGUltimateEntry(Entry):
+  pass
+
+
+@dataclasses.dataclass
+class KeybaseEntry(Entry):
+  pass
+
+
+@dataclasses.dataclass
+class Config:
+
+  entries: List[Entry]
+
+  @classmethod
+  def load(cls) -> 'Config':
+    cfg = configparser.ConfigParser()
+    cfg.read([_LOCATION])
+    entries: List[Entry] = []
+    for section in cfg.sections():
+      entry_type = cfg.get(section, 'type')
+      if entry_type == 'web_keys_api':
+        entries.append(KeysAPIEntry(
+            name=section,
+            url=cfg.get(section, 'url'),
+        ))
+      elif entry_type == 'gpg_key':
+        entries.append(GPGKeyEntry(
+            name=section,
+            fingerprint=cfg.get(section, 'fingerprint'),
+        ))
+      elif entry_type == 'gpg_ultimate_trust':
+        entries.append(GPGUltimateEntry(name=section))
+      elif entry_type == 'keybase_pgp':
+        entries.append(KeybaseEntry(name=section))
+      else:
+        raise ValueError(f'Unknown key source {entry_type} in [{section}]')
+    return cls(entries=entries)
