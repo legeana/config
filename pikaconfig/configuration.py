e04bfba101b8ee88c4e5b2e57a8063020168f8dd
Lisa White <legeana@liri.ie>
2021-08-02 22:53:19 +0100

    Move SystemSetupEntry and children into pikaconfig.system_entry

--- pikaconfig/configuration.py
+++ pikaconfig/configuration.py
@@ -4,25 +4,11 @@ import pathlib
 import shlex
 import shutil
 import sys
-from typing import Collection, Dict, List, Type
+from typing import Collection, Dict, List
 
 from . import entry
-from . import system
+from . import system_entry
 from . import util
-
-
-class SystemSetupEntry(entry.Entry):
-
-  DISTROS: List[str] = []
-
-  def __init__(self, args: List[str]):
-    raise NotImplementedError
-
-  def system_setup(self) -> None:
-    raise NotImplementedError
-
-  def install(self, record: entry.PathRecorder) -> None:
-    pass
 
 
 class PostInstallHook(entry.Entry):
@@ -76,151 +62,6 @@ class FileEntry(entry.Entry):
 
   src: pathlib.Path
   dst: pathlib.Path
-
-
-@dataclasses.dataclass
-class SystemCommandEntry(SystemSetupEntry):
-
-  args: List[str]
-
-  def system_setup(self) -> None:
-    # TODO implement a confirmation
-    util.verbose_check_call('sudo', *self.args)
-
-
-class SystemCommandParser(entry.Parser):
-
-  def parse(self, command: str, args: List[str]) -> entry.Entry:
-    self.check_supported(command)
-    return SystemCommandEntry(args=args)
-
-  @property
-  def supported_commands(self) -> Collection[str]:
-    return ['sudo']
-
-
-class AnyPackageEntry(SystemSetupEntry):
-
-  def __init__(self, entries):
-    self._entries = entries
-
-  def system_setup(self) -> None:
-    for entry in self._entries:
-      entry.system_setup()
-
-
-class AnyPackageParser(entry.Parser):
-
-  @property
-  def _package_managers(self) -> List[Type[SystemSetupEntry]]:
-    return [
-        PacmanPackageEntry,
-        AptPackageEntry,
-        BrewPackageEntry,
-    ]
-
-  def parse(self, command: str, args: List[str]) -> entry.Entry:
-    self.check_supported(command)
-    sysid = system.os_id()
-    entries = [
-        cls(args) for cls in self._package_managers
-        if sysid in cls.DISTROS
-    ]
-    return AnyPackageEntry(entries)
-
-  @property
-  def supported_commands(self) -> Collection[str]:
-    return ['install_system_package']
-
-
-@dataclasses.dataclass
-class PacmanPackageEntry(SystemSetupEntry):
-
-  DISTROS = ['arch']
-  names: List[str]
-
-  def system_setup(self) -> None:
-    if system.os_id() not in self.DISTROS:
-      return
-    util.verbose_check_call('sudo', 'pacman', '-S', '--', *self.names)
-
-
-class PacmanPackageParser(entry.Parser):
-
-  def parse(self, command: str, args: List[str]) -> entry.Entry:
-    self.check_supported(command)
-    return PacmanPackageEntry(args)
-
-  @property
-  def supported_commands(self) -> Collection[str]:
-    return ['install_pacman_package']
-
-
-@dataclasses.dataclass
-class AptPackageEntry(SystemSetupEntry):
-
-  DISTROS = ['debian', 'ubuntu']
-  names: List[str]
-
-  def system_setup(self) -> None:
-    if system.os_id() not in self.DISTROS:
-      return
-    util.verbose_check_call('sudo', 'apt', 'install', '--', *self.names)
-
-
-class AptPackageParser(entry.Parser):
-
-  def parse(self, command: str, args: List[str]) -> entry.Entry:
-    self.check_supported(command)
-    return AptPackageEntry(args)
-
-  @property
-  def supported_commands(self) -> Collection[str]:
-    return ['install_apt_package']
-
-
-@dataclasses.dataclass
-class BrewPackageEntry(SystemSetupEntry):
-
-  DISTROS = ['darwin']
-  names: List[str]
-
-  def system_setup(self) -> None:
-    if system.os_id() not in self.DISTROS:
-      return
-    util.verbose_check_call('brew', 'install', '--', *self.names)
-
-
-class BrewPackageParser(entry.Parser):
-
-  def parse(self, command: str, args: List[str]) -> entry.Entry:
-    self.check_supported(command)
-    return BrewPackageEntry(args)
-
-  @property
-  def supported_commands(self) -> Collection[str]:
-    return ['install_brew_package']
-
-
-@dataclasses.dataclass
-class PipPackageEntry(SystemSetupEntry):
-
-  names: List[str]
-
-  def system_setup(self) -> None:
-    # system check is not necessary because pip is cross platform
-    util.verbose_check_call('pip', 'install', '--user', '--', *self.names)
-
-
-class PipPackageParser(entry.Parser):
-
-  def parse(self, command: str, args: List[str]) -> entry.Entry:
-    self.check_supported(command)
-    return PipPackageEntry(args)
-
-  @property
-  def supported_commands(self) -> Collection[str]:
-    return ['install_pip_user_package']
 
 
 class SymlinkEntry(FileEntry):
@@ -332,12 +173,12 @@ class Manifest(entry.Entry):
     self._parsers = CombinedParser(
         ManifestParser(root=root, prefix=self._prefix),
         SetPrefixParser(root=root, prefix=self._prefix),
-        SystemCommandParser(),
-        AnyPackageParser(),
-        PacmanPackageParser(),
-        AptPackageParser(),
-        BrewPackageParser(),
-        PipPackageParser(),
+        system_entry.SystemCommandParser(),
+        system_entry.AnyPackageParser(),
+        system_entry.PacmanPackageParser(),
+        system_entry.AptPackageParser(),
+        system_entry.BrewPackageParser(),
+        system_entry.PipPackageParser(),
         SymlinkParser(root=root, prefix=self._prefix),
         CopyParser(root=root, prefix=self._prefix),
         ExecPostHookParser(root=root, prefix=self._prefix),
---
+++ pikaconfig/system_entry.py
@@ -0,0 +1,165 @@
+import dataclasses
+from typing import Collection, List, Type
+
+from . import entry
+from . import system
+from . import util
+
+
+class SystemSetupEntry(entry.Entry):
+
+  DISTROS: List[str] = []
+
+  def __init__(self, args: List[str]):
+    raise NotImplementedError
+
+  def system_setup(self) -> None:
+    raise NotImplementedError
+
+  def install(self, record: entry.PathRecorder) -> None:
+    pass
+
+
+@dataclasses.dataclass
+class SystemCommandEntry(SystemSetupEntry):
+
+  args: List[str]
+
+  def system_setup(self) -> None:
+    # TODO implement a confirmation
+    util.verbose_check_call('sudo', *self.args)
+
+
+class SystemCommandParser(entry.Parser):
+
+  def parse(self, command: str, args: List[str]) -> entry.Entry:
+    self.check_supported(command)
+    return SystemCommandEntry(args=args)
+
+  @property
+  def supported_commands(self) -> Collection[str]:
+    return ['sudo']
+
+
+class AnyPackageEntry(SystemSetupEntry):
+
+  def __init__(self, entries):
+    self._entries = entries
+
+  def system_setup(self) -> None:
+    for entry in self._entries:
+      entry.system_setup()
+
+
+class AnyPackageParser(entry.Parser):
+
+  @property
+  def _package_managers(self) -> List[Type[SystemSetupEntry]]:
+    return [
+        PacmanPackageEntry,
+        AptPackageEntry,
+        BrewPackageEntry,
+    ]
+
+  def parse(self, command: str, args: List[str]) -> entry.Entry:
+    self.check_supported(command)
+    sysid = system.os_id()
+    entries = [
+        cls(args) for cls in self._package_managers
+        if sysid in cls.DISTROS
+    ]
+    return AnyPackageEntry(entries)
+
+  @property
+  def supported_commands(self) -> Collection[str]:
+    return ['install_system_package']
+
+
+@dataclasses.dataclass
+class PacmanPackageEntry(SystemSetupEntry):
+
+  DISTROS = ['arch']
+  names: List[str]
+
+  def system_setup(self) -> None:
+    if system.os_id() not in self.DISTROS:
+      return
+    util.verbose_check_call('sudo', 'pacman', '-S', '--', *self.names)
+
+
+class PacmanPackageParser(entry.Parser):
+
+  def parse(self, command: str, args: List[str]) -> entry.Entry:
+    self.check_supported(command)
+    return PacmanPackageEntry(args)
+
+  @property
+  def supported_commands(self) -> Collection[str]:
+    return ['install_pacman_package']
+
+
+@dataclasses.dataclass
+class AptPackageEntry(SystemSetupEntry):
+
+  DISTROS = ['debian', 'ubuntu']
+  names: List[str]
+
+  def system_setup(self) -> None:
+    if system.os_id() not in self.DISTROS:
+      return
+    util.verbose_check_call('sudo', 'apt', 'install', '--', *self.names)
+
+
+class AptPackageParser(entry.Parser):
+
+  def parse(self, command: str, args: List[str]) -> entry.Entry:
+    self.check_supported(command)
+    return AptPackageEntry(args)
+
+  @property
+  def supported_commands(self) -> Collection[str]:
+    return ['install_apt_package']
+
+
+@dataclasses.dataclass
+class BrewPackageEntry(SystemSetupEntry):
+
+  DISTROS = ['darwin']
+  names: List[str]
+
+  def system_setup(self) -> None:
+    if system.os_id() not in self.DISTROS:
+      return
+    util.verbose_check_call('brew', 'install', '--', *self.names)
+
+
+class BrewPackageParser(entry.Parser):
+
+  def parse(self, command: str, args: List[str]) -> entry.Entry:
+    self.check_supported(command)
+    return BrewPackageEntry(args)
+
+  @property
+  def supported_commands(self) -> Collection[str]:
+    return ['install_brew_package']
+
+
+@dataclasses.dataclass
+class PipPackageEntry(SystemSetupEntry):
+
+  names: List[str]
+
+  def system_setup(self) -> None:
+    # system check is not necessary because pip is cross platform
+    util.verbose_check_call('pip', 'install', '--user', '--', *self.names)
+
+
+class PipPackageParser(entry.Parser):
+
+  def parse(self, command: str, args: List[str]) -> entry.Entry:
+    self.check_supported(command)
+    return PipPackageEntry(args)
+
+  @property
+  def supported_commands(self) -> Collection[str]:
+    return ['install_pip_user_package']
