73837325bdb444ffe28049a25d5ccbc26c8bc673
Lisa White <legeana@liri.ie>
2021-08-06 21:32:54 +0100

    Revert "Add PuTTY + tmux Ctrl+Left and Ctrl+Right key bindings"
    This reverts commit 862e54ad6bcc999380181195a03949f31a6cae0e.
    The reverted commit was wrong, it maps Left and Right keys.

--- apps/fish/functions/fish_user_key_bindings.fish
+++ apps/fish/functions/fish_user_key_bindings.fish
@@ -1,11 +1,6 @@
-# fish_key_reader can be used to generate bind commands
-
 function fish_user_key_bindings
     bind \cd delete-char
     # PuTTY
     bind \eOD backward-word
     bind \eOC forward-word
-    # PuTTY + tmux
-    bind \e\[D backward-word
-    bind \e\[C forward-word
 end
