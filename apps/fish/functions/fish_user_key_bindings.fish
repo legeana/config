fe4ba92f3cb2fc15fae4edf778c4c1158ff5d958
Lisa White <legeana@liri.ie>
2021-08-08 15:19:22 +0100

    Add useful resources for PuTTY configuration

--- apps/fish/functions/fish_user_key_bindings.fish
+++ apps/fish/functions/fish_user_key_bindings.fish
@@ -4,3 +4,15 @@ function fish_user_key_bindings
     bind \eOD backward-word
     bind \eOC forward-word
 end
+
+# Useful resources:
+# - https://blog.sanctum.geek.nz/putty-configuration/
+#   - TERM=putty-256color comes from ncurses-term package
+#   - Set Connection > Data >> Terminal-type string to putty-256color.
+#   - Window > Colours >> Allow terminal to use xterm 256-colour mode.
+#   - Window > Translation >> Remote character set = UTF-8.
+#   - Window > Appearance >> Font = Consolas.
+#   - Terminal > Bell = None.
+#   - Window > Selection >> Action of mouse buttons: xterm
+# - https://github.com/jblaine/solarized-and-modern-putty/blob/master/putty-modern-256color.reg
+# - https://puttytray.goeswhere.com/
