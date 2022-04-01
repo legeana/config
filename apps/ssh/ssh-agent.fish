4064f0b55b15c576782858355f2af417fcff7d9e
Lisa White <legeana@liri.ie>
2022-04-01 07:19:22 -0700

    ssh: don't override SSH_AUTH_SOCK in an ssh session

--- apps/ssh/ssh-agent.fish
+++ apps/ssh/ssh-agent.fish
@@ -1,4 +1,7 @@
-# only use on systemd distros
-if test -S "$XDG_RUNTIME_DIR/ssh-agent.socket"
+# only use if the socket is available and not in an ssh session
+if test -S "$XDG_RUNTIME_DIR/ssh-agent.socket";
+        and test -z "$SSH_TTY";
+        and test -z "$SSH_CONNECTION";
+        and test -z "$SSH_CLIENT"
     set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 end
