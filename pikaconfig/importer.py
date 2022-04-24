2a6e3ac09ec55e7eac2b1c661bfe968aa96d612c
Lisa White <legeana@liri.ie>
2022-04-24 10:57:29 -0700

    pikaconfig.importer.render(): remove keyword parameter

--- pikaconfig/importer.py
+++ pikaconfig/importer.py
@@ -1,23 +1,20 @@
 import pathlib
 from typing import TextIO
 
-KEYWORD = '#import'
+IMPORT = '#import'
 
 
-def _render(*, prefix: pathlib.Path, src: pathlib.Path,
-            out: TextIO, keyword: str) -> None:
+def _render(*, prefix: pathlib.Path, src: pathlib.Path, out: TextIO) -> None:
   with open(src, 'rt') as tmpl:
     for line in tmpl:
-      if not line.startswith(keyword):
+      if not line.startswith(IMPORT):
         out.write(line)
         continue
-      include = line[len(keyword):].strip()
+      include = line[len(IMPORT):].strip()
       include_file = prefix / pathlib.Path(include)
-      _render(prefix=include_file.parent, src=include_file,
-              out=out, keyword=keyword)
+      _render(prefix=include_file.parent, src=include_file, out=out)
 
 
-def render(*, src: pathlib.Path, dst: pathlib.Path,
-           keyword: str = KEYWORD) -> None:
+def render(*, src: pathlib.Path, dst: pathlib.Path) -> None:
     with open(dst, 'wt') as out:
-      _render(prefix=dst.parent, src=src, out=out, keyword=keyword)
+      _render(prefix=dst.parent, src=src, out=out)
