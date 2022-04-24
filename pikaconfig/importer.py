5b50aeceb5f3cd8fa0c9a4bba667e8cb972ff2c6
Lisa White <legeana@liri.ie>
2022-04-24 15:33:34 -0700

    pikaconfig.importer: add #import_tree command

--- pikaconfig/importer.py
+++ pikaconfig/importer.py
@@ -1,18 +1,43 @@
+import enum
+import os
 import pathlib
-from typing import TextIO
+from typing import Iterable, TextIO
 
-IMPORT = '#import'
+
+@enum.unique
+class Keywords(enum.Enum):
+  IMPORT = '#import'
+  IMPORT_TREE = '#import_tree'
+
+  @classmethod
+  def all(cls) -> Iterable[str]:
+    # reverse order by length is important so that we match the longest keyword
+    return sorted(cls, key=lambda v: len(v.value), reverse=True)
 
 
 def _render(*, prefix: pathlib.Path, src: pathlib.Path, out: TextIO) -> None:
   with open(src, 'rt') as tmpl:
     for line in tmpl:
-      if not line.startswith(IMPORT):
+      keyword = None
+      arg = None
+      for kw in Keywords.all():
+        if line.startswith(kw.value):
+          keyword = kw
+          arg = line[len(kw.value):].strip()
+          break
+      if keyword is None:
         out.write(line)
-        continue
-      include = line[len(IMPORT):].strip()
-      include_file = prefix / pathlib.Path(include)
-      _render(prefix=include_file.parent, src=include_file, out=out)
+      elif keyword is Keywords.IMPORT:
+        include_file = prefix / pathlib.Path(arg)
+        _render(prefix=include_file.parent, src=include_file, out=out)
+      elif keyword is Keywords.IMPORT_TREE:
+        include_dir = prefix / pathlib.Path(arg)
+        for _root, _, files in os.walk(include_dir):
+          root = pathlib.Path(_root)
+          for f in files:
+            _render(prefix=root, src=root / f, out=out)
+      else:
+        raise Exception(f'Logic error: unknown keyword {keyword!r}')
 
 
 def render(*, src: pathlib.Path, dst: pathlib.Path) -> None:
