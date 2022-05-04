b10bf3a5c1fb8d251b84a718b5f97d4a28b0d342
Lisa White <legeana@liri.ie>
2022-05-04 14:27:39 +0100

    fish: add workaround to support dircolors for alacritty

--- apps/fish/config.d/90_dircolors.fish
+++ apps/fish/config.d/90_dircolors.fish
@@ -1,3 +1,8 @@
 must-have-command dircolors
 
 eval (dircolors --csh)
+
+# dircolors doesn't always support alacritty
+if test "$TERM" = alacritty -a -z "$LS_COLORS"
+    eval (env TERM=xterm-256color dircolors --csh)
+end
