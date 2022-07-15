226d19418acbd5fb0e6ed835a9eb52f59143fc66
Lisa White <legeana@liri.ie>
2022-07-15 09:58:21 -0700

    ssh-agent: don't override SSH_AUTH_SOCK in TMUX sessions

--- apps/ssh/ssh-agent.fish
+++ apps/ssh/ssh-agent.fish
@@ -2,6 +2,7 @@
 if test -S "$XDG_RUNTIME_DIR/ssh-agent.socket";
         and test -z "$SSH_TTY";
         and test -z "$SSH_CONNECTION";
-        and test -z "$SSH_CLIENT"
+        and test -z "$SSH_CLIENT";
+        and test -z "$TMUX"
     set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 end
