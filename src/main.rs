7e005bbe6a4e2a2b6f036067750ff250609bda5c
Lisa White <legeana@liri.ie>
2022-08-29 15:04:24 +0100

    pikaconfig: implement reload on update

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ use anyhow::{anyhow, Result};
 use clap::{Args, Parser, Subcommand};
 
 use std::env;
+use std::ffi::OsString;
 use std::path::{Path, PathBuf};
+
+const NO_UPDATE_ENV: &str = "PIKACONFIG_NO_UPDATE";
 
 fn config_root() -> Result<PathBuf> {
     let exe_path = env::current_exe()?;
@@ -37,6 +40,19 @@ enum Commands {
     Uninstall {},
 }
 
+fn reload() -> Result<()> {
+    let setup = config_root()?.join("setup");
+    println!("Restarting: $ {} {:?}", setup.display(), env::args_os().skip(1).collect::<Vec<OsString>>());
+    let exit_status = std::process::Command::new(&setup)
+        .args(env::args_os().skip(1))
+        .env(NO_UPDATE_ENV, "yes")
+        .status()?;
+    if !exit_status.success() {
+        return Err(anyhow!("failed to run {}", setup.display()));
+    }
+    return Ok(());
+}
+
 fn debug(root: &Path) -> Result<()> {
     let repos = layout::repositories(root)?;
     for repo in repos {
@@ -51,11 +67,16 @@ fn main() -> Result<()> {
     let args = Cli::parse();
     match args.command {
         Commands::Install {} => {
-            if !args.no_update {
+            let no_update = args.no_update || match env::var(NO_UPDATE_ENV) {
+                Ok(_) => true,
+                Err(_) => false,
+            };
+            if !no_update {
                 let need_restart = layout::update(&root)?;
                 if need_restart {
-                    // TODO use env variable to set no_update
-                    println!("need restart");
+                    // This process is considered replaced.
+                    // Don't do anything here.
+                    return reload();
                 }
             }
             debug(&root)?;
