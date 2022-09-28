5d123bd1e0e5452b9e74f99e6985b466ceb10dd4
Lisa White <legeana@liri.ie>
2022-09-29 00:15:43 +0100

    ssh: detect and use 1password-agent if available

--- apps/ssh/ssh-agent.fish
+++ apps/ssh/ssh-agent.fish
@@ -1,8 +1,11 @@
 # only use if the socket is available and not in an ssh session
-if test -S "$XDG_RUNTIME_DIR/ssh-agent.socket";
-        and test -z "$SSH_TTY";
+if test -z "$SSH_TTY";
         and test -z "$SSH_CONNECTION";
         and test -z "$SSH_CLIENT";
         and test -z "$TMUX"
-    set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+    if test -S "$HOME/.1password/agent.sock"
+        set -gx SSH_AUTH_SOCK "$HOME/.1password/agent.sock"
+    else if test -S "$XDG_RUNTIME_DIR/ssh-agent.socket"
+        set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+    end
 end
