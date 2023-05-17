18157bde3c6e8b517939551588d35ad93ee2f83c
Lisa White <legeana@liri.ie>
2023-05-17 22:48:45 +0100

    fish: don't try to start new tmux session in tm, print an error instead

--- apps/fish/functions/tm.fish
+++ apps/fish/functions/tm.fish
@@ -1,4 +1,9 @@
 function tm -a session
+    if set -q TMUX
+        echo Already in tmux session >&2
+        verbose-eval tmux lsc
+        return 1
+    end
     if test -z "$session"
         set session default
     end
