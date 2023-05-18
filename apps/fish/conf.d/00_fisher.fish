36b98cf573023681ae61ad47791a20aa4915d098
Lisa White <legeana@liri.ie>
2023-05-18 11:38:08 +0100

    fish: install fisher lazily on first use

--- apps/fish/conf.d/00_fisher.fish
+++ apps/fish/conf.d/00_fisher.fish
@@ -1,2 +1,16 @@
 set -g fisher_path ~/.local/share/fisher
 load_plugin $fisher_path
+
+function __get_fisher
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish |
+        source &&
+        fisher install jorgebucaran/fisher
+end
+
+if ! functions -q fisher
+    function fisher
+        __get_fisher
+        load_plugin $fisher_path
+        fisher $argv
+    end
+end
