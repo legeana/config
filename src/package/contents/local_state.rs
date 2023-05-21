9207fc924f1d0965b21593a6335bce66351f6f24
Lisa White <legeana@liri.ie>
2023-05-21 15:11:34 +0100

    pikaconfig: add local_state::DirectoryState

--- src/package/contents/local_state.rs
+++ src/package/contents/local_state.rs
@@ -6,7 +6,11 @@ use base64::Engine;
 use sha2::{Digest, Sha256};
 
 use super::file_util;
-use crate::registry::Registry;
+use crate::{registry::Registry, package::Module};
+
+type StateType = &'static str;
+const FILE_STATE: StateType = "output";
+const DIR_STATE: StateType = "dirs";
 
 fn path_hash(path: &Path) -> Result<PathBuf> {
     let path_str = path
@@ -21,13 +25,13 @@ fn path_hash(path: &Path) -> Result<PathBuf> {
     Ok(URL_SAFE.encode(result).into())
 }
 
-fn state_path(path: &Path) -> Result<PathBuf> {
+fn state_path(path: &Path, state_type: StateType) -> Result<PathBuf> {
     let hash = path_hash(path).with_context(|| format!("unable to make hash of {path:?}"))?;
     // TODO: Windows/MacOS
     let output_state = dirs::state_dir()
         .ok_or_else(|| anyhow!("failed to get state dir"))?
         .join("pikaconfig")
-        .join("output");
+        .join(state_type);
     Ok(output_state.join(hash))
 }
 
@@ -38,11 +42,20 @@ pub struct FileState {
 
 impl FileState {
     pub fn new(dst: PathBuf) -> Result<Self> {
-        let state =
-            state_path(&dst).with_context(|| format!("failed to build state path for {dst:?}"))?;
+        let state = state_path(&dst, FILE_STATE)
+            .with_context(|| format!("failed to build state path for {dst:?}"))?;
         Ok(Self { state, dst })
     }
-    pub fn install(&self, registry: &mut dyn Registry) -> Result<()> {
+    pub fn path(&self) -> &Path {
+        &self.state
+    }
+    pub fn link(&self) -> &Path {
+        &self.dst
+    }
+}
+
+impl Module for FileState {
+    fn install(&self, registry: &mut dyn Registry) -> Result<()> {
         let state_dir = self
             .state
             .parent()
@@ -51,10 +64,31 @@ impl FileState {
             .with_context(|| format!("failed to create {state_dir:?}"))?;
         file_util::make_symlink(registry, &self.state, &self.dst)
     }
+}
+
+pub struct DirectoryState {
+    state: PathBuf,
+    dst: PathBuf,
+}
+
+impl DirectoryState {
+    pub fn new(dst: PathBuf) -> Result<Self> {
+        let state = state_path(&dst, DIR_STATE)
+            .with_context(|| format!("failed to build state path for {dst:?}"))?;
+        Ok(Self { state, dst })
+    }
     pub fn path(&self) -> &Path {
         &self.state
     }
     pub fn link(&self) -> &Path {
         &self.dst
+    }
+}
+
+impl Module for DirectoryState {
+    fn install(&self, registry: &mut dyn Registry) -> Result<()> {
+        std::fs::create_dir_all(&self.state)
+            .with_context(|| format!("failed to create {:?}", &self.state))?;
+        file_util::make_symlink(registry, &self.state, &self.dst)
     }
 }
