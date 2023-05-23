8f48f633e5ab4c646b68f65e8a4a7e61a7b1cc0c
Lisa White <legeana@liri.ie>
2023-05-23 02:45:35 +0100

    pikaconfig: add git_clone branch parser

--- src/package/contents/git_clone.rs
+++ src/package/contents/git_clone.rs
@@ -9,9 +9,31 @@ use crate::registry::Registry;
 pub struct GitCloneParser {}
 
 const COMMAND: &str = "git_clone";
+const BRANCH_SEP: char = '#';
+
+struct Remote {
+    url: String,
+    #[allow(dead_code)]
+    branch: Option<String>,
+}
+
+impl Remote {
+    fn new(addr: &str) -> Self {
+        match addr.rsplit_once(BRANCH_SEP) {
+            Some((url, branch)) => Self {
+                url: url.to_owned(),
+                branch: Some(branch.to_owned()),
+            },
+            None => Self {
+                url: addr.to_owned(),
+                branch: None,
+            },
+        }
+    }
+}
 
 struct GitClone {
-    url: String,
+    remote: Remote,
     output: local_state::DirectoryState,
 }
 
@@ -20,7 +42,7 @@ impl GitClone {
         git_utils::git_hard_pull(self.output.path())
     }
     fn clone(&self) -> Result<()> {
-        git_utils::git_clone(&self.url, self.output.path())
+        git_utils::git_clone(&self.remote.url, self.output.path())
     }
     fn is_empty(&self) -> Result<bool> {
         let count = std::fs::read_dir(self.output.path())
@@ -58,8 +80,9 @@ impl parser::Parser for GitCloneParser {
         COMMAND
     }
     fn help(&self) -> &'static str {
-        "git_clone <url> <directory>
-           git clone <url> into a local storage and installs a symlink to it"
+        "git_clone <url>[#<branch>] <directory>
+           git clone <url> into a local storage and installs a symlink to it
+           if <branch> is specified clone <branch> instead of default HEAD"
     }
     fn parse(
         &self,
@@ -75,9 +98,35 @@ impl parser::Parser for GitCloneParser {
         let output = local_state::DirectoryState::new(dst.clone())
             .with_context(|| format!("failed to create DirectoryState from {dst:?}"))?;
         configuration.modules.push(Box::new(GitClone {
-            url: url.to_owned(),
+            remote: Remote::new(url),
             output,
         }));
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_remote_without_branch() {
+        let remote = Remote::new("http://github.com/example/repo.git");
+        assert_eq!(remote.url, "http://github.com/example/repo.git");
+        assert_eq!(remote.branch, None);
+    }
+
+    #[test]
+    fn test_remote_with_branch() {
+        let remote = Remote::new("http://github.com/example/repo.git#branch");
+        assert_eq!(remote.url, "http://github.com/example/repo.git");
+        assert_eq!(remote.branch, Some("branch".to_owned()));
+    }
+
+    #[test]
+    fn test_remote_with_branch_and_hashes() {
+        let remote = Remote::new("http://github.com/#example/repo.git#branch");
+        assert_eq!(remote.url, "http://github.com/#example/repo.git");
+        assert_eq!(remote.branch, Some("branch".to_owned()));
     }
 }
