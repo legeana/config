9e65f2e0c4e3477b58c821fd4bcc41534e0b36e0
Lisa White <legeana@liri.ie>
2023-05-26 14:08:22 +0100

    pikaconfig: support escaping the newline in MANIFEST

--- src/package/contents/parser.rs
+++ src/package/contents/parser.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Lines};
+use std::iter::Enumerate;
 use std::path::Path;
 
 use anyhow::{anyhow, Context, Result};
@@ -7,6 +8,66 @@ use anyhow::{anyhow, Context, Result};
 use crate::package::Module;
 
 use super::builder;
+
+const LINE_CONT: char = '\\';
+
+struct LineIterator<B: BufRead> {
+    lines: Enumerate<Lines<B>>,
+    end: bool,
+}
+
+impl<B: BufRead> LineIterator<B> {
+    fn new(lines: Lines<B>) -> Self {
+        Self {
+            lines: lines.enumerate(),
+            end: false,
+        }
+    }
+}
+
+fn is_cont(line: &str) -> bool {
+    // TODO: support "line\\"
+    line.ends_with(LINE_CONT)
+}
+
+impl<B: BufRead> Iterator for LineIterator<B> {
+    type Item = (usize, Result<String>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.end {
+            return None;
+        }
+        let mut buffer: Vec<String> = Vec::new();
+        let mut buf_line_num = 0;
+        loop {
+            match self.lines.next() {
+                Some((line_idx, Ok(line))) => {
+                    if buffer.is_empty() {
+                        buf_line_num = line_idx;
+                    }
+                    if is_cont(&line) {
+                        buffer.push(line[..line.len() - 1].into());
+                    } else {
+                        buffer.push(line);
+                        break;
+                    }
+                },
+                Some((line_idx, Err(err))) => {
+                    self.end = true;
+                    return Some((line_idx, Err(err.into())));
+                },
+                None => {
+                    self.end = true;
+                    break;
+                },
+            }
+        }
+        if buffer.is_empty() {
+            return None;
+        }
+        Some((buf_line_num, Ok(buffer.join(""))))
+    }
+}
 
 fn parse_line(state: &mut builder::State, line: &str) -> Result<Option<Box<dyn Module>>> {
     if line.is_empty() || line.starts_with('#') {
@@ -22,7 +83,7 @@ pub fn parse(state: &mut builder::State, manifest_path: &Path) -> Result<Vec<Box
         File::open(manifest_path).with_context(|| format!("failed to open {manifest_path:?}"))?;
     let reader = BufReader::new(manifest);
     let mut modules: Vec<Box<dyn Module>> = Vec::new();
-    for (line_idx, line_or) in reader.lines().enumerate() {
+    for (line_idx, line_or) in LineIterator::new(reader.lines()) {
         let line_num = line_idx + 1;
         let line = line_or
             .with_context(|| format!("failed to read line {line_num} from {manifest_path:?}"))?;
