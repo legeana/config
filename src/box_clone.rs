fcc5d7b437e27d0ffad9610e6b3d341fd456835e
Lisa White <legeana@liri.ie>
2023-06-15 03:48:28 +0100

    pikaconfig: add BoxClone test

--- src/box_clone.rs
+++ src/box_clone.rs
@@ -10,3 +10,20 @@ where
         Box::new(self.clone())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Clone)]
+    struct Test {
+        x: String,
+    }
+
+    #[test]
+    fn test_box_clone() {
+        let t = Test { x: "test".to_owned() };
+        let b = t.box_clone();
+        assert_eq!(b.x, "test".to_owned());
+    }
+}
