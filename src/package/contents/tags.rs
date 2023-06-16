c8ea58151e8573a3c829ee7e7b559dba9fcf4e6a
Lisa White <legeana@liri.ie>
2023-06-16 15:34:44 +0100

    pikaconfig: implement RequiresBuilder and ConflictsBuilder

--- src/package/contents/tags.rs
+++ src/package/contents/tags.rs
@@ -7,6 +7,24 @@ use crate::tag_util;
 use super::builder;
 use super::util;
 
+#[derive(Debug)]
+struct RequiresBuilder {
+    tags: Vec<String>,
+}
+
+impl builder::Builder for RequiresBuilder {
+    fn build(&self, state: &mut builder::State) -> Result<Option<Box<dyn Module>>> {
+        for tag in self.tags.iter() {
+            let has_tag =
+                tag_util::has_tag(tag).with_context(|| format!("failed to check tag {tag}"))?;
+            if !has_tag {
+                state.enabled = false;
+            }
+        }
+        Ok(None)
+    }
+}
+
 #[derive(Clone)]
 struct RequiresParser;
 
@@ -20,12 +38,25 @@ impl builder::Parser for RequiresParser {
                 do not process current directory if any of the tags is not present
         ", command=self.name()}
     }
-    fn build(&self, state: &mut builder::State, args: &[&str]) -> Result<Option<Box<dyn Module>>> {
+    fn parse(&self, args: &[&str]) -> Result<Box<dyn builder::Builder>> {
         let (_, tags) = util::multiple_args(&self.name(), args, 0)?;
-        for tag in tags.iter() {
+        Ok(Box::new(RequiresBuilder {
+            tags: tags.iter().map(|&s| s.to_owned()).collect(),
+        }))
+    }
+}
+
+#[derive(Debug)]
+struct ConflictsBuilder {
+    tags: Vec<String>,
+}
+
+impl builder::Builder for ConflictsBuilder {
+    fn build(&self, state: &mut builder::State) -> Result<Option<Box<dyn Module>>> {
+        for tag in self.tags.iter() {
             let has_tag =
                 tag_util::has_tag(tag).with_context(|| format!("failed to check tag {tag}"))?;
-            if !has_tag {
+            if has_tag {
                 state.enabled = false;
             }
         }
@@ -46,16 +77,11 @@ impl builder::Parser for ConflictsParser {
                 do not process current directory if any of the tags is present
         ", command=self.name()}
     }
-    fn build(&self, state: &mut builder::State, args: &[&str]) -> Result<Option<Box<dyn Module>>> {
+    fn parse(&self, args: &[&str]) -> Result<Box<dyn builder::Builder>> {
         let (_, tags) = util::multiple_args(&self.name(), args, 0)?;
-        for tag in tags.iter() {
-            let has_tag =
-                tag_util::has_tag(tag).with_context(|| format!("failed to check tag {tag}"))?;
-            if has_tag {
-                state.enabled = false;
-            }
-        }
-        Ok(None)
+        Ok(Box::new(ConflictsBuilder {
+            tags: tags.iter().map(|&s| s.to_owned()).collect(),
+        }))
     }
 }
 
