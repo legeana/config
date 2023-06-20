e07bd8f209af6fbcd412b9ad844e53436bddff05
Lisa White <legeana@liri.ie>
2023-06-20 16:17:17 +0100

    pikaconfig: add SimpleFilter helper

--- src/tera_helper.rs
+++ src/tera_helper.rs
@@ -1,13 +1,21 @@
 use std::{collections::HashMap, marker::PhantomData};
 
-use anyhow::Result;
-use serde::de::value::MapDeserializer;
-use serde::Deserialize;
-use tera::{Function, Value};
+use anyhow::{Context, Result};
+use serde::de::{value::MapDeserializer, DeserializeOwned};
+use tera::{Filter, Function, Value};
+
+fn deserialize_args<T>(args: &HashMap<String, Value>) -> Result<T>
+where
+    T: DeserializeOwned + Send + Sync,
+{
+    // https://github.com/serde-rs/serde/issues/1739
+    T::deserialize(MapDeserializer::new(args.clone().into_iter()))
+        .context("failed to deserialize args")
+}
 
 pub trait SimpleFunction<Params>
 where
-    Params: Deserialize<'static> + Send + Sync,
+    Params: DeserializeOwned + Send + Sync,
 {
     fn call(&self, args: &Params) -> Result<Value>;
 }
@@ -15,7 +23,7 @@ where
 pub struct WrappedFunction<T, Params>
 where
     T: SimpleFunction<Params> + Send + Sync,
-    Params: Deserialize<'static> + Send + Sync,
+    Params: DeserializeOwned + Send + Sync,
 {
     wrapped: T,
     phantom: PhantomData<Params>,
@@ -24,12 +32,14 @@ where
 impl<T, Params> Function for WrappedFunction<T, Params>
 where
     T: SimpleFunction<Params> + Send + Sync,
-    Params: Deserialize<'static> + Send + Sync,
+    Params: DeserializeOwned + Send + Sync,
 {
     fn call(&self, args: &HashMap<String, Value>) -> tera::Result<Value> {
-        // https://github.com/serde-rs/serde/issues/1739
-        let args = Params::deserialize(MapDeserializer::new(args.clone().into_iter()))?;
-        match self.wrapped.call(&args) {
+        let wrap_result = || {
+            let args: Params = deserialize_args(args)?;
+            self.wrapped.call(&args)
+        };
+        match wrap_result() {
             Ok(result) => Ok(result),
             Err(err) => Err(tera::Error::msg(err.to_string())),
         }
@@ -39,10 +49,48 @@ where
 pub fn wrap_function<T, Params>(func: T) -> WrappedFunction<T, Params>
 where
     T: SimpleFunction<Params> + Send + Sync,
-    Params: Deserialize<'static> + Send + Sync,
+    Params: DeserializeOwned + Send + Sync,
 {
     WrappedFunction {
         wrapped: func,
         phantom: PhantomData,
+    }
+}
+
+pub trait SimpleFilter<V, Params>
+where
+    Params: DeserializeOwned + Send + Sync,
+    V: DeserializeOwned + Send + Sync,
+{
+    fn call(&self, value: &V, args: &Params) -> Result<Value>;
+}
+
+pub struct WrappedFilter<T, V, Params>
+where
+    T: SimpleFilter<V, Params> + Send + Sync,
+    V: DeserializeOwned + Send + Sync,
+    Params: DeserializeOwned + Send + Sync,
+{
+    wrapped: T,
+    value: PhantomData<V>,
+    params: PhantomData<Params>,
+}
+
+impl<T, V, Params> Filter for WrappedFilter<T, V, Params>
+where
+    T: SimpleFilter<V, Params> + Send + Sync,
+    V: DeserializeOwned + Send + Sync,
+    Params: DeserializeOwned + Send + Sync,
+{
+    fn filter(&self, value: &Value, args: &HashMap<String, Value>) -> tera::Result<Value> {
+        let wrap_result = || {
+            let value = serde_json::from_value(value.to_owned())?;
+            let args: Params = deserialize_args(args)?;
+            self.wrapped.call(&value, &args)
+        };
+        match wrap_result() {
+            Ok(result) => Ok(result),
+            Err(err) => Err(tera::Error::msg(err.to_string())),
+        }
     }
 }
