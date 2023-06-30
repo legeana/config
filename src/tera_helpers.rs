5ac34b10df594fcf12d17d24092c6a6401b90423
Lisa White <legeana@liri.ie>
2023-06-30 03:15:40 +0100

    pikaconfig: remove unnecessary Result from tera_helpers::register()

--- src/tera_helpers.rs
+++ src/tera_helpers.rs
@@ -1,12 +1,9 @@
-use anyhow::Result;
-
 use crate::quote;
 use crate::tera_helper;
 
-pub fn register(tera: &mut tera::Tera) -> Result<()> {
+pub fn register(tera: &mut tera::Tera) {
     tera.register_filter(
         "enquote",
         tera_helper::wrap_nil_filter(|text: &String| Ok(quote::enquote(text))),
     );
-    Ok(())
 }
