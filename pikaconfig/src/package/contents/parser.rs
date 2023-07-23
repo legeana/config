d298216b6ec30547b030e50e0de1a3ef9b978044
Lisa White <legeana@liri.ie>
2023-07-23 12:21:24 +0100

    pikaconfig: factor parser::parse_statements() out of parser::parse()

--- pikaconfig/src/package/contents/parser.rs
+++ pikaconfig/src/package/contents/parser.rs
@@ -29,24 +29,26 @@ impl engine::Statement for ParsedStatement {
 pub fn parse(workdir: &Path, manifest_path: &Path) -> Result<Vec<engine::StatementBox>> {
     let manifest = std::fs::read_to_string(manifest_path)
         .with_context(|| format!("failed to read {manifest_path:?}"))?;
-    let mut builders: Vec<engine::StatementBox> = Vec::new();
     let manifest_ast = ast::Manifest::parse(manifest_path, manifest)
         .with_context(|| format!("failed to parse {manifest_path:?}"))?;
-    for statement in manifest_ast.statements {
-        match statement {
-            ast::Statement::Command(cmd) => {
-                builders.push(parse_command(
-                    workdir,
-                    manifest_path,
-                    &cmd,
-                )?);
+    parse_statements(workdir, manifest_path, manifest_ast.statements.iter())
+}
+
+pub fn parse_statements<'a>(
+    workdir: &Path,
+    manifest_path: &Path,
+    statements: impl Iterator<Item = &'a ast::Statement>,
+) -> Result<Vec<engine::StatementBox>> {
+    statements
+        .map(|statement| -> Result<engine::StatementBox> {
+            match statement {
+                ast::Statement::Command(cmd) => parse_command(workdir, manifest_path, &cmd),
+                ast::Statement::IfStatement(_if_st) => {
+                    todo!();
+                }
             }
-            ast::Statement::IfStatement(_if_st) => {
-                todo!();
-            }
-        }
-    }
-    Ok(builders)
+        })
+        .collect()
 }
 
 pub fn parse_command(
@@ -55,9 +57,8 @@ pub fn parse_command(
     cmd: &ast::Invocation,
 ) -> Result<engine::StatementBox> {
     let line = cmd.to_string();
-    let statement = engine::new_command(workdir, &cmd.name, &cmd.args).with_context(|| {
-        format!("failed to parse line {manifest_path:?}: {line}")
-    })?;
+    let statement = engine::new_command(workdir, &cmd.name, &cmd.args)
+        .with_context(|| format!("failed to parse line {manifest_path:?}: {line}"))?;
     Ok(Box::new(ParsedStatement {
         line,
         manifest_path: manifest_path.to_owned(),
