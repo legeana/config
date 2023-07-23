ff8664dafe90ba22d4240022abcd3b311dd91174
Lisa White <legeana@liri.ie>
2023-07-23 13:04:09 +0100

    pikaconfig: add is_os conditions

--- pikaconfig/src/package/contents/if_os.rs
+++ pikaconfig/src/package/contents/if_os.rs
@@ -4,33 +4,65 @@ use crate::module::ModuleBox;
 use crate::tera_helper;
 
 use super::args::Arguments;
-use super::engine;
+use super::engine::{self, ConditionBuilder};
 use super::inventory;
 
 use anyhow::Result;
 use indoc::formatdoc;
 
-fn is_os(os: &str) -> bool {
-    // We don't have to check this in runtime as this never changes.
-    // In fact, it's even beneficial to check this during build to support *Prefix.
-    os == std::env::consts::FAMILY || os == std::env::consts::OS
+#[derive(Debug)]
+struct IsOs(&'static str);
+
+impl IsOs {
+    fn check(&self) -> bool {
+        // We don't have to check this in runtime as this never changes.
+        // In fact, it's even beneficial to check this during build to support *Prefix.
+        self.0 == std::env::consts::FAMILY || self.0 == std::env::consts::OS
+    }
+}
+
+impl engine::Condition for IsOs {
+    fn eval(&self, _ctx: &engine::Context) -> Result<bool> {
+        Ok(self.check())
+    }
+}
+
+#[derive(Clone)]
+struct IsOsBuilder(&'static str);
+
+impl engine::ConditionBuilder for IsOsBuilder {
+    fn name(&self) -> String {
+        format!("is_{}", self.0)
+    }
+    fn help(&self) -> String {
+        formatdoc! {"
+            {condition}
+                true if os is {os}
+        ", condition=self.name(), os=self.0}
+    }
+    fn build(&self, _workdir: &Path, args: &Arguments) -> Result<engine::ConditionBox> {
+        args.expect_no_args(self.name())?;
+        Ok(Box::new(IsOs(self.0)))
+    }
+}
+
+impl inventory::RenderHelper for IsOsBuilder {
+    fn register_render_helper(&self, tera: &mut tera::Tera) {
+        let name = self.name();
+        let is_os = IsOs(self.0);
+        tera.register_function(&name, tera_helper::wrap_nil(move || Ok(is_os.check())));
+    }
 }
 
 #[derive(Debug)]
 struct IfOsStatement {
-    os: &'static str,
+    is_os: IsOs,
     cmd: engine::StatementBox,
-}
-
-impl IfOsStatement {
-    fn is_os(&self) -> bool {
-        is_os(self.os)
-    }
 }
 
 impl engine::Statement for IfOsStatement {
     fn eval(&self, ctx: &mut engine::Context) -> Result<Option<ModuleBox>> {
-        if !self.is_os() {
+        if !self.is_os.check() {
             return Ok(None);
         }
         self.cmd.eval(ctx)
@@ -63,29 +95,17 @@ impl engine::CommandBuilder for IfOsBuilder {
         assert!(empty.is_empty());
         let cmd_args: Vec<_> = cmd_args.iter().map(String::as_str).collect();
         Ok(Box::new(IfOsStatement {
-            os: self.os,
+            is_os: IsOs(self.os),
             cmd: engine::parse_args(workdir, &cmd_args)?,
         }))
     }
 }
 
-impl inventory::RenderHelper for IfOsBuilder {
-    fn register_render_helper(&self, tera: &mut tera::Tera) {
-        let name = format!("is_{}", self.os);
-        let os = self.os.to_owned();
-        tera.register_function(&name, tera_helper::wrap_nil(move || Ok(is_os(&os))));
-    }
-}
-
 pub fn register(registry: &mut dyn inventory::Registry) {
-    let parsers = [
-        IfOsBuilder { os: "macos" },
-        IfOsBuilder { os: "linux" },
-        IfOsBuilder { os: "unix" },
-        IfOsBuilder { os: "windows" },
-    ];
-    for parser in parsers {
-        registry.register_command(Box::new(parser.clone()));
-        registry.register_render_helper(Box::new(parser));
+    let oss = ["macos", "linux", "unix", "windows"];
+    for os in oss {
+        registry.register_command(Box::new(IfOsBuilder { os }));
+        registry.register_condition(Box::new(IsOsBuilder(os)));
+        registry.register_render_helper(Box::new(IsOsBuilder(os)));
     }
 }
