dc9c8a98860b51f851a54f90376bf7c40fd31f04
Lisa White <legeana@liri.ie>
2023-07-23 13:07:40 +0100

    pikaconfig: remove if_os commands

--- pikaconfig/src/package/contents/if_os.rs
+++ pikaconfig/src/package/contents/if_os.rs
@@ -1,6 +1,5 @@
 use std::path::Path;
 
-use crate::module::ModuleBox;
 use crate::tera_helper;
 
 use super::args::Arguments;
@@ -27,7 +26,7 @@ impl engine::Condition for IsOs {
     }
 }
 
-#[derive(Clone)]
+#[derive(Copy, Clone)]
 struct IsOsBuilder(&'static str);
 
 impl engine::ConditionBuilder for IsOsBuilder {
@@ -54,58 +53,15 @@ impl inventory::RenderHelper for IsOsBuilder {
     }
 }
 
-#[derive(Debug)]
-struct IfOsStatement {
-    is_os: IsOs,
-    cmd: engine::StatementBox,
-}
-
-impl engine::Statement for IfOsStatement {
-    fn eval(&self, ctx: &mut engine::Context) -> Result<Option<ModuleBox>> {
-        if !self.is_os.check() {
-            return Ok(None);
-        }
-        self.cmd.eval(ctx)
-    }
-}
-
-#[derive(Clone)]
-struct IfOsBuilder {
-    os: &'static str,
-}
-
-impl IfOsBuilder {
-    fn command(&self) -> String {
-        format!("if_{}", self.os)
-    }
-}
-
-impl engine::CommandBuilder for IfOsBuilder {
-    fn name(&self) -> String {
-        self.command()
-    }
-    fn help(&self) -> String {
-        formatdoc! {"
-            {command} <command> [<args>...]
-                execute a MANIFEST <command> only if os (or family) is {os}
-        ", os=self.os, command=self.command()}
-    }
-    fn build(&self, workdir: &Path, args: &Arguments) -> Result<engine::StatementBox> {
-        let (empty, cmd_args) = args.expect_variadic_args(self.command(), 0)?;
-        assert!(empty.is_empty());
-        let cmd_args: Vec<_> = cmd_args.iter().map(String::as_str).collect();
-        Ok(Box::new(IfOsStatement {
-            is_os: IsOs(self.os),
-            cmd: engine::parse_args(workdir, &cmd_args)?,
-        }))
-    }
-}
-
 pub fn register(registry: &mut dyn inventory::Registry) {
-    let oss = ["macos", "linux", "unix", "windows"];
-    for os in oss {
-        registry.register_command(Box::new(IfOsBuilder { os }));
-        registry.register_condition(Box::new(IsOsBuilder(os)));
-        registry.register_render_helper(Box::new(IsOsBuilder(os)));
+    let builders = [
+        IsOsBuilder("macos"),
+        IsOsBuilder("linux"),
+        IsOsBuilder("unix"),
+        IsOsBuilder("windows"),
+    ];
+    for builder in builders {
+        registry.register_condition(Box::new(builder));
+        registry.register_render_helper(Box::new(builder));
     }
 }
