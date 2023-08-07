7f985b21de16f735bd05b88b3f671029fed5b500
Lisa White <legeana@liri.ie>
2023-08-07 13:41:42 +0100

    pikaconfig: support Argument in fetch_into and fetch_exe_into

--- pikaconfig/src/package/contents/fetch.rs
+++ pikaconfig/src/package/contents/fetch.rs
@@ -6,7 +6,7 @@ use indoc::formatdoc;
 use crate::module::{Module, ModuleBox, Rules};
 use crate::registry::Registry;
 
-use super::args::Arguments;
+use super::args::{Argument, Arguments};
 use super::engine;
 use super::inventory;
 use super::local_state;
@@ -65,14 +65,14 @@ impl Module for FetchInto {
 
 #[derive(Debug)]
 struct FetchIntoStatement {
-    filename: String,
+    filename: Argument,
     url: String,
     executable: bool,
 }
 
 impl engine::Statement for FetchIntoStatement {
     fn eval(&self, ctx: &mut engine::Context) -> Result<Option<ModuleBox>> {
-        let dst = ctx.dst_path(&self.filename);
+        let dst = ctx.dst_path(ctx.expand_arg(&self.filename)?);
         let output = local_state::FileState::new(dst.clone())
             .with_context(|| format!("failed to create FileState from {dst:?}"))?;
         Ok(Some(Box::new(FetchInto {
@@ -100,7 +100,7 @@ impl engine::CommandBuilder for FetchIntoBuilder {
     fn build(&self, _workdir: &Path, args: &Arguments) -> Result<engine::Command> {
         let (filename, url) = args.expect_double_arg(self.name())?;
         Ok(engine::Command::new_statement(FetchIntoStatement {
-            filename: filename.expect_raw().context("filename")?.to_owned(),
+            filename: filename.clone(),
             url: url.expect_raw().context("url")?.to_owned(),
             executable: false,
         }))
@@ -124,7 +124,7 @@ impl engine::CommandBuilder for FetchExeIntoBuilder {
     fn build(&self, _workdir: &Path, args: &Arguments) -> Result<engine::Command> {
         let (filename, url) = args.expect_double_arg(self.name())?;
         Ok(engine::Command::new_statement(FetchIntoStatement {
-            filename: filename.expect_raw().context("filename")?.to_owned(),
+            filename: filename.clone(),
             url: url.expect_raw().context("url")?.to_owned(),
             executable: true,
         }))
