e889e8f43ab3fdc22d3d04a432985fbf6866bca7
Lisa White <legeana@liri.ie>
2023-08-07 14:07:34 +0100

    pikaconfig: support Argument for git_clone's output

--- pikaconfig/src/package/contents/git_clone.rs
+++ pikaconfig/src/package/contents/git_clone.rs
@@ -7,7 +7,7 @@ use crate::git_utils;
 use crate::module::{Module, ModuleBox, Rules};
 use crate::registry::Registry;
 
-use super::args::Arguments;
+use super::args::{Argument, Arguments};
 use super::engine;
 use super::inventory;
 use super::local_state;
@@ -74,12 +74,12 @@ impl Module for GitClone {
 #[derive(Debug)]
 struct GitCloneStatement {
     url: String,
-    dst: String,
+    dst: Argument,
 }
 
 impl engine::Statement for GitCloneStatement {
     fn eval(&self, ctx: &mut engine::Context) -> Result<Option<ModuleBox>> {
-        let dst = ctx.dst_path(&self.dst);
+        let dst = ctx.dst_path(ctx.expand_arg(&self.dst)?);
         let output = local_state::DirectoryState::new(dst.clone())
             .with_context(|| format!("failed to create DirectoryState from {dst:?}"))?;
         Ok(Some(Box::new(GitClone {
@@ -107,7 +107,7 @@ impl engine::CommandBuilder for GitCloneBuilder {
         let (url, dst) = args.expect_double_arg(self.name())?;
         Ok(engine::Command::new_statement(GitCloneStatement {
             url: url.expect_raw().context("url")?.to_owned(),
-            dst: dst.expect_raw().context("destination")?.to_owned(),
+            dst: dst.clone(),
         }))
     }
 }
