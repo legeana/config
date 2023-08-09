1aa96644131e4e8d7d611b9ad35de76d0c2dd66e
Lisa White <legeana@liri.ie>
2023-08-09 16:51:02 +0100

    pikaconfig: make unzip quiet and overwrite files without asking

--- pikaconfig/src/unarchiver/unzip.rs
+++ pikaconfig/src/unarchiver/unzip.rs
@@ -16,6 +16,8 @@ impl super::Unarchiver for Unzip {
     fn unarchive(&self, src: &Path, dst: &Path) -> Result<()> {
         process_utils::run_verbose(
             std::process::Command::new("unzip")
+                .arg("-o")
+                .arg("-q")
                 .arg(src)
                 .arg("-d")
                 .arg(dst),
