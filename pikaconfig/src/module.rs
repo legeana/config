cedae47c89fdefdfc151bb7db4c44c02abe49a26
Lisa White <legeana@liri.ie>
2023-08-14 22:40:45 +0100

    pikaconfig: add module::impl_wrap!() and use it for WrappedModule

--- pikaconfig/src/module.rs
+++ pikaconfig/src/module.rs
@@ -128,38 +128,43 @@ impl_for_part!(
     self, (self.0) (self.1) (self.2)
 );
 
+macro_rules! impl_wrap {
+    ($t:ident, ($($wrap:tt)*), ($sel:ident .$($module:tt)*)) => {
+        impl<T: Module> Module for $t<T> {
+            fn pre_uninstall(&$sel, rules: &Rules) -> Result<()> {
+                $($wrap)*("pre_uninstall", || $sel.$($module)*.pre_uninstall(rules))
+            }
+            fn pre_install(&$sel, rules: &Rules, registry: &mut dyn Registry) -> Result<()> {
+                $($wrap)*("pre_install", || $sel.$($module)*.pre_install(rules, registry))
+            }
+            fn install(&$sel, rules: &Rules, registry: &mut dyn Registry) -> Result<()> {
+                $($wrap)*("install", || $sel.$($module)*.install(rules, registry))
+            }
+            fn post_install(&$sel, rules: &Rules, registry: &mut dyn Registry) -> Result<()> {
+                $($wrap)*("post_install", || $sel.$($module)*.post_install(rules, registry))
+            }
+            fn system_install(&$sel, rules: &Rules) -> Result<()> {
+                $($wrap)*("system_install", || $sel.$($module)*.system_install(rules))
+            }
+        }
+    };
+}
+
 struct WrappedModule<T: Module> {
     module: T,
     error_context: String,
 }
 
-impl<T: Module> Module for WrappedModule<T> {
-    fn pre_uninstall(&self, rules: &Rules) -> Result<()> {
-        self.module
-            .pre_uninstall(rules)
-            .with_context(|| format!("failed pre_uninstall in {:?}", self.error_context))
-    }
-    fn pre_install(&self, rules: &Rules, registry: &mut dyn Registry) -> Result<()> {
-        self.module
-            .pre_install(rules, registry)
-            .with_context(|| format!("failed pre_install in {:?}", self.error_context))
-    }
-    fn install(&self, rules: &Rules, registry: &mut dyn Registry) -> Result<()> {
-        self.module
-            .install(rules, registry)
-            .with_context(|| format!("failed install in {:?}", self.error_context))
-    }
-    fn post_install(&self, rules: &Rules, registry: &mut dyn Registry) -> Result<()> {
-        self.module
-            .post_install(rules, registry)
-            .with_context(|| format!("failed post_install in {:?}", self.error_context))
-    }
-    fn system_install(&self, rules: &Rules) -> Result<()> {
-        self.module
-            .system_install(rules)
-            .with_context(|| format!("failed system_install in {:?}", self.error_context))
+impl<T: Module> WrappedModule<T> {
+    fn wrap<F>(&self, method: &str, f: F) -> Result<()>
+    where
+        F: FnOnce() -> Result<()>,
+    {
+        f().with_context(|| format!("failed {method} in {:?}", self.error_context))
     }
 }
+
+impl_wrap!(WrappedModule, (self.wrap), (self.module));
 
 pub fn wrap<T: Module + 'static>(module: T, error_context: String) -> ModuleBox {
     Box::new(WrappedModule {
