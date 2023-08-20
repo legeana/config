097d4998b5e6338c94d628eeafa6bc4fc0b02304
Lisa White <legeana@liri.ie>
2023-08-20 20:59:11 +0100

    pikaconfig: factor FileList out of FileRegistry

--- pikaconfig/src/file_registry.rs
+++ pikaconfig/src/file_registry.rs
@@ -7,53 +7,69 @@ use std::{
 
 use anyhow::{anyhow, Context, Result};
 
-pub struct FileRegistry {
-    path: PathBuf,
-}
+struct FileList(PathBuf);
 
-impl FileRegistry {
-    pub fn new(path: PathBuf) -> Self {
-        Self { path }
-    }
-}
-
-impl Registry for FileRegistry {
-    fn register_user_file(&mut self, path: &Path, _file_type: FileType) -> Result<()> {
-        let mut paths = self.user_files()?;
+impl FileList {
+    fn push(&mut self, path: &Path) -> Result<()> {
+        let mut paths = self.list()?;
         paths.push(path.to_owned());
-        let db = std::fs::File::create(&self.path)
-            .with_context(|| format!("unable to open {:?}", self.path))?;
+        let db = std::fs::File::create(&self.0)
+            .with_context(|| format!("unable to open {:?}", self.0))?;
         let mut writer = std::io::BufWriter::new(db);
         for path in paths {
             let s = path
                 .to_str()
                 .ok_or_else(|| anyhow!("{path:?} is not a valid unicode"))?;
             writeln!(&mut writer, "{}", s)
-                .with_context(|| format!("failed to write to {:?}", self.path))?;
+                .with_context(|| format!("failed to write to {:?}", self.0))?;
         }
         writer
             .flush()
-            .with_context(|| format!("unable to write to {:?}", self.path))
+            .with_context(|| format!("unable to write to {:?}", self.0))
     }
-    fn user_files(&self) -> Result<Vec<PathBuf>> {
-        if !self.path.exists() {
+    fn list(&self) -> Result<Vec<PathBuf>> {
+        if !self.0.exists() {
             return Ok(Vec::new());
         }
-        if !self.path.is_file() {
-            return Err(anyhow!("{:?} registry must be a file", self.path));
+        if !self.0.is_file() {
+            return Err(anyhow!("{:?} registry must be a file", self.0));
         }
-        let data = std::fs::read_to_string(&self.path)
-            .with_context(|| format!("failed to read {:?}", self.path))?;
+        let data = std::fs::read_to_string(&self.0)
+            .with_context(|| format!("failed to read {:?}", self.0))?;
         Ok(data
             .split('\n')
             .filter(|s| !s.is_empty())
             .map(PathBuf::from)
             .collect())
     }
-    fn clear_user_files(&mut self) -> Result<()> {
-        match std::fs::remove_file(&self.path) {
+    fn clear(&mut self) -> Result<()> {
+        match std::fs::remove_file(&self.0) {
             Err(err) if err.kind() == ErrorKind::NotFound => Ok(()),
-            r => r.with_context(|| format!("failed to remove {:?}", self.path)),
+            r => r.with_context(|| format!("failed to remove {:?}", self.0)),
         }
+    }
+}
+
+pub struct FileRegistry {
+    user_files: FileList,
+}
+
+impl FileRegistry {
+    pub fn new(path: PathBuf) -> Self {
+        Self {
+            user_files: FileList(path),
+        }
+    }
+}
+
+impl Registry for FileRegistry {
+    fn register_user_file(&mut self, path: &Path, _file_type: FileType) -> Result<()> {
+        self.user_files.push(path)
+    }
+    fn user_files(&self) -> Result<Vec<PathBuf>> {
+        self.user_files.list()
+    }
+    fn clear_user_files(&mut self) -> Result<()> {
+        self.user_files.clear()
     }
 }
