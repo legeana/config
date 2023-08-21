3380cba19afde6fd63249e5eae3674b9c187c9ca
Lisa White <legeana@liri.ie>
2023-08-21 02:36:57 +0100

    pikaconfig: add `tags` condition

--- pikaconfig/src/package/contents/tags.rs
+++ pikaconfig/src/package/contents/tags.rs
@@ -92,7 +92,48 @@ impl engine::CommandBuilder for ConflictsBuilder {
     }
 }
 
+#[derive(Debug)]
+struct TagsCondition(Vec<String>);
+
+impl engine::Condition for TagsCondition {
+    fn eval(&self, _ctx: &engine::Context) -> Result<bool> {
+        for tag in self.0.iter() {
+            let has_tag =
+                tag_util::has_tag(tag).with_context(|| format!("failed to check tag {tag}"))?;
+            if !has_tag {
+                return Ok(false);
+            }
+        }
+        Ok(true)
+    }
+}
+
+#[derive(Clone)]
+struct TagsBuilder;
+
+impl engine::ConditionBuilder for TagsBuilder {
+    fn name(&self) -> String {
+        "tags".to_owned()
+    }
+    fn help(&self) -> String {
+        formatdoc! {"
+            {command} [<tag>...]
+                Returns true if all tags are matching.
+                Both tag=value and tag!=value are supported.
+        ", command=self.name()}
+    }
+    fn build(&self, _workdir: &Path, args: &Arguments) -> Result<engine::ConditionBox> {
+        let tags = args.expect_any_args(self.name())?;
+        let tags: Vec<_> = tags
+            .iter()
+            .map(|t| t.expect_raw().context("tag").map(str::to_string))
+            .collect::<Result<_>>()?;
+        Ok(Box::new(TagsCondition(tags)))
+    }
+}
+
 pub fn register(registry: &mut dyn inventory::Registry) {
-    registry.register_command(Box::new(RequiresBuilder {}));
-    registry.register_command(Box::new(ConflictsBuilder {}));
+    registry.register_command(Box::new(RequiresBuilder));
+    registry.register_command(Box::new(ConflictsBuilder));
+    registry.register_condition(Box::new(TagsBuilder));
 }
