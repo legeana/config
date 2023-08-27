a42485ff38b9ad9434986692c8bc9dede1ff96a2
Lisa White <legeana@liri.ie>
2023-08-27 19:06:07 +0100

    pikaconfig: add DependencySatisficer::AnyCommand and AllCommands

--- pikaconfig/src/package/satisficer.rs
+++ pikaconfig/src/package/satisficer.rs
@@ -1,8 +1,7 @@
 use std::path::PathBuf;
 
+use anyhow::{Context, Result};
 use serde::Deserialize;
-
-use anyhow::Result;
 
 use crate::command;
 
@@ -26,12 +25,35 @@ where
 #[serde(deny_unknown_fields, untagged)]
 pub enum DependencySatisficer {
     Command { command: PathBuf },
+    AnyCommand { any_command: Vec<PathBuf> },
+    AllCommands { all_commands: Vec<PathBuf> },
 }
 
 impl Satisficer for DependencySatisficer {
     fn is_satisfied(&self) -> Result<bool> {
         match self {
-            DependencySatisficer::Command { command } => command::is_command(command),
+            DependencySatisficer::Command { command } => command::is_command(command)
+                .with_context(|| format!("failed to check if {command:?} is available")),
+            DependencySatisficer::AnyCommand { any_command } => {
+                for cmd in any_command {
+                    if command::is_command(cmd)
+                        .with_context(|| format!("failed to check if {cmd:?} is available"))?
+                    {
+                        return Ok(true);
+                    }
+                }
+                Ok(false)
+            }
+            DependencySatisficer::AllCommands { all_commands } => {
+                for cmd in all_commands {
+                    if !command::is_command(cmd)
+                        .with_context(|| format!("failed to check if {cmd:?} is available"))?
+                    {
+                        return Ok(false);
+                    }
+                }
+                Ok(true)
+            }
         }
     }
 }
