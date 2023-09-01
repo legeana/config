629cf349c89536e0051c3f99d5211ec9fecb6d16
Lisa White <legeana@liri.ie>
2023-09-01 23:38:00 +0100

    pikaconfig: use try_fold() where appropriate

--- pikaconfig/src/iter_util.rs
+++ pikaconfig/src/iter_util.rs
@@ -10,10 +10,10 @@ pub fn to_option<T, Item>(collection: T) -> Result<Option<Item>>
 where
     T: IntoIterator<Item = Item>,
 {
-    let iter = collection.into_iter();
-    iter.fold(Ok(None), |acc, value| match acc {
-        Ok(None) => Ok(Some(value)),
-        Ok(Some(_)) => Err(anyhow!("too many elements")),
-        Err(err) => Err(err),
-    })
+    collection
+        .into_iter()
+        .try_fold(None, |acc, value| match acc {
+            None => Ok(Some(value)),
+            Some(_) => Err(anyhow!("too many elements")),
+        })
 }
