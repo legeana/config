953266e68f64f3721373f2460aadea3740a0e385
Lisa White <legeana@liri.ie>
2024-01-25 08:15:50 -0800

    fish: update per-session per-client-tty SSH_AUTH_SOCK in tmux

--- apps/fish/conf.d/30_tmux.fish
+++ apps/fish/conf.d/30_tmux.fish
@@ -2,18 +2,19 @@
 set -g _TM_TMP "$HOME/.local/state/tmux-ssh-agent"
 set -g _TM_PLACEHOLDER /dev/null
 
-function _tm_env_for_session -a session -a env
-    echo "$_TM_TMP/$USER.$session.$env"
+function _tm_env_for_session -a session -a tty -a env
+    set -l tty (string replace -a / _ "$tty")
+    echo "$_TM_TMP/$USER.$session.$tty.$env"
 end
 
-function _set_up_tmux_environment
-    set -l session (tmux display-message -p '#S')
-    set -gx SSH_AUTH_SOCK (_tm_env_for_session "$session" "SSH_AUTH_SOCK")
+function _set_up_tmux_environment --on-event fish_prompt
+    if ! set -q TMUX
+        return
+    end
+    set -l session (tmux display-message -p '#{session_name}')
+    set -l tty (tmux display-message -p '#{client_tty}')
+    set -gx SSH_AUTH_SOCK (_tm_env_for_session "$session" "$tty" "SSH_AUTH_SOCK")
     set -e FWD_SSH_AUTH_SOCK
-end
-
-if set -q TMUX
-    _set_up_tmux_environment
 end
 
 function _tm_cleanup
@@ -32,9 +33,9 @@ end
 
 # The functions below operate under the assumption that tmux will not be used
 # by more than one user simultaneously. The last connection wins.
-function _tm_forward_socket -a session -a env -a value
+function _tm_forward_socket -a session -a tty -a env -a value
     mkdir -p -m 700 "$_TM_TMP"
-    set path (_tm_env_for_session "$session" "$env")
+    set path (_tm_env_for_session "$session" "$tty" "$env")
     set dst "$value"
     if test -z "$dst"
         set dst $_TM_PLACEHOLDER
@@ -44,11 +45,12 @@ end
 
 function _tm_forward -a session
     _tm_cleanup
+    set -l tty (tty)
     # Restore FWD_SSH_AUTH_SOCK and forward it as SSH_AUTH_SOCK if possible.
     # FWD_SSH_AUTH_SOCK if set points to the original agent socket.
     if ! set -q FWD_SSH_AUTH_SOCK || ! test -S $FWD_SSH_AUTH_SOCK
-        _tm_forward_socket $session SSH_AUTH_SOCK "$SSH_AUTH_SOCK"
+        _tm_forward_socket $session "$tty" SSH_AUTH_SOCK "$SSH_AUTH_SOCK"
     else
-        _tm_forward_socket $session SSH_AUTH_SOCK "$FWD_SSH_AUTH_SOCK"
+        _tm_forward_socket $session "$tty" SSH_AUTH_SOCK "$FWD_SSH_AUTH_SOCK"
     end
 end
