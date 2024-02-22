2686ce9ec8b53dca51f8ae43919c4c7afdc09c2f
Lisa White <legeana@liri.ie>
2024-02-22 21:59:03 +0000

    pikaconfig: update config root if pikaconfig isn't embedded

--- pikaconfig/src/layout.rs
+++ pikaconfig/src/layout.rs
@@ -11,6 +11,7 @@ use anyhow::{Context, Result};
 const APPS: &str = "apps";
 const OVERLAY: &str = "overlay.d";
 const GIT_DIR: &str = ".git";
+const PIKACONFIG_DIR: &str = "pikaconfig";
 
 fn read_dir_sorted(path: &Path) -> Result<Vec<DirEntry>> {
     let mut paths = path
@@ -115,8 +116,13 @@ fn update_repository(root: &Path) -> Result<bool> {
 }
 
 pub fn update(root: &Path) -> Result<()> {
-    // TODO: run if installed independently from configs
-    // update_repository(root)?;
+    let pikaconfig_dir = root.join(PIKACONFIG_DIR);
+    let pikaconfig_is_embedded = pikaconfig_dir
+        .try_exists()
+        .with_context(|| format!("failed to check if {:?} exists", pikaconfig_dir))?;
+    if !pikaconfig_is_embedded {
+        update_repository(root)?;
+    }
     for overlay in overlay_dirs(root)? {
         update_repository(&overlay)?;
     }
