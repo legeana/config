5c8fcdb575c337f2e9ca79c7d513a5bafe1ce443
Lisa White <legeana@liri.ie>
2024-03-07 01:04:16 +0000

    bootstrap: add cli parsing

--- bootstrap/src/main.rs
+++ bootstrap/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, World!");
+use anyhow::{bail, Result};
+
+use pikaconfig_bootstrap::cli;
+
+fn main() -> Result<()> {
+    let args = cli::parse();
+    env_logger::Builder::new()
+        .filter_level(match args.verbose {
+            0 => log::LevelFilter::Off,
+            1 => log::LevelFilter::Error,
+            2 => log::LevelFilter::Warn,
+            3 => log::LevelFilter::Info,
+            4 => log::LevelFilter::Debug,
+            5 => log::LevelFilter::Trace,
+            6.. => bail!("invalid log level: {}", args.verbose),
+        })
+        .default_format()
+        .format_timestamp(None)
+        .format_target(false)
+        .try_init()?;
+    bail!("Not implemented yet");
 }
