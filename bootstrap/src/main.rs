8059c342e2a1bddac1f283ec8cdc4a70c4ba43a4
Lisa White <legeana@liri.ie>
2024-03-07 23:00:20 +0000

    bootstrap: forward to pikaconfig

--- bootstrap/src/main.rs
+++ bootstrap/src/main.rs
@@ -1,11 +1,22 @@
-use anyhow::{bail, Result};
+use anyhow::Result;
 
 use pikaconfig_bootstrap::cli;
 use pikaconfig_bootstrap::logconfig;
+use pikaconfig_bootstrap::process_utils;
+
+fn run_pikaconfig() -> Result<()> {
+    process_utils::run(
+        std::process::Command::new("cargo")
+            .arg("run")
+            .arg("--package=pikaconfig")
+            .arg("--")
+            .args(std::env::args().skip(1)), // Skip arg0.
+    )
+}
 
 fn main() -> Result<()> {
     let args = cli::parse();
     logconfig::init(args.verbose)?;
     // Main code.
-    bail!("Not implemented yet");
+    run_pikaconfig()
 }
