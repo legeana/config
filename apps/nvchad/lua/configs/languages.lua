aaf2ce9ceceb9721d808c22742d80c370c1a2318
Lisa White <legeana@liri.ie>
2024-03-17 15:44:02 -0700

    nvchad: add typescript (javascript) and commented-out java

--- apps/nvchad/lua/configs/languages.lua
+++ apps/nvchad/lua/configs/languages.lua
@@ -6,8 +6,10 @@ local packages = {
   { lsp = "cssls", mason = "css-lsp", ts = {"css"} },
   { lsp = "gopls", mason = "gopls", ts = {"go"} },
   { lsp = "html", mason = "html-lsp", ts = {"html"} },
+  -- { lsp = "java_language_server", mason = "java-language-server", ts = {"java"} },
   { lsp = "pylsp", mason = "python-lsp-server", ts = {"python"} },
   { lsp = "rust_analyzer", mason = "rust-analyzer", ts = {"rust"} },
+  { lsp = "tsserver", mason = "typescript-language-server", ts = {"typescript"} },
 }
 
 M.lsp = vim.tbl_map(function(e) return e.lsp end, packages)
