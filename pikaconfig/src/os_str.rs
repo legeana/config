f3656c3a2210d9371089eb2ff9cf3aad2b3e1088
Lisa White <legeana@liri.ie>
2024-07-28 00:01:54 +0100

    pikaconfig: add Windows support to os_str

--- pikaconfig/src/os_str.rs
+++ pikaconfig/src/os_str.rs
@@ -6,6 +6,37 @@ use std::ffi::OsString;
 
 use anyhow::Result;
 
+#[cfg(any(test, windows))]
+mod windows {
+    use anyhow::{anyhow, Result};
+
+    pub(super) fn to_wide(b: [u8; 2]) -> u16 {
+        u16::from_le_bytes(b)
+    }
+
+    pub(super) fn from_wide(b: u16) -> [u8; 2] {
+        b.to_le_bytes()
+    }
+
+    pub(super) fn to_wide_vec(b: Vec<u8>) -> Result<Vec<u16>> {
+        if b.len() % 2 != 0 {
+            return Err(anyhow!("must have even size"));
+        }
+        let mut r = Vec::<u16>::with_capacity(b.len() / 2);
+        for c in b.chunks(2) {
+            if c.len() != 2 {
+                return Err(anyhow!("uneven number of bytes"));
+            }
+            r.push(to_wide([c[0], c[1]]));
+        }
+        Ok(r)
+    }
+
+    pub(super) fn from_wide_iter(w: impl Iterator<Item = u16>) -> Vec<u8> {
+        w.map(from_wide).flatten().collect()
+    }
+}
+
 #[cfg(unix)]
 mod platform {
     use std::ffi::OsString;
@@ -24,7 +55,22 @@ mod platform {
 
 #[cfg(windows)]
 mod platform {
-    compile_error!("Windows is not yet supported");
+    use std::ffi::OsString;
+    use std::os::windows::ffi::OsStrExt;
+    use std::os::windows::ffi::OsStringExt;
+
+    use anyhow::Result;
+
+    use super::windows;
+
+    pub(super) fn from_vec(vec: Vec<u8>) -> Result<OsString> {
+        let w = windows::to_wide_vec(vec)?;
+        Ok(OsString::from_wide(&w))
+    }
+
+    pub(super) fn to_vec(os_str: OsString) -> Vec<u8> {
+        windows::from_wide_iter(os_str.encode_wide())
+    }
 }
 
 pub fn from_vec(vec: Vec<u8>) -> Result<OsString> {
@@ -40,6 +86,41 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     use super::*;
+
+    #[test]
+    fn test_to_wide() {
+        assert_eq!(windows::to_wide([0x00, 0x10]), 0x1000);
+        assert_eq!(windows::to_wide([0x10, 0x00]), 0x0010);
+        assert_eq!(windows::to_wide([0x01, 0x02]), 0x0201);
+    }
+
+    #[test]
+    fn test_from_wide() {
+        assert_eq!(windows::from_wide(0x1000), [0x00, 0x10]);
+        assert_eq!(windows::from_wide(0x0010), [0x10, 0x00]);
+        assert_eq!(windows::from_wide(0x1020), [0x20, 0x10]);
+    }
+
+    #[test]
+    fn test_to_wide_vec() {
+        assert_eq!(
+            windows::to_wide_vec(vec![0x00, 0x01, 0x02, 0x03]).unwrap(),
+            vec![0x0100, 0x0302],
+        );
+    }
+
+    #[test]
+    fn test_to_wide_vec_odd() {
+        assert!(windows::to_wide_vec(vec![0x00]).is_err());
+    }
+
+    #[test]
+    fn test_from_wide_iter() {
+        assert_eq!(
+            windows::from_wide_iter(vec![0x0100, 0x0302].into_iter()),
+            vec![0x00, 0x01, 0x02, 0x03],
+        );
+    }
 
     #[test]
     fn test_conversions() {
