cc1e2c611a9ff8378e2a24ddc4f91d5b30c18807
Lisa White <legeana@liri.ie>
2024-12-19 00:43:40 +0000

    nvchad: add listchars configuration

--- apps/nvchad/lua/options.lua
+++ apps/nvchad/lua/options.lua
@@ -2,5 +2,7 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+o.list = true
+o.listchars = "tab:>.,trail:$,extends:#,nbsp:."
 -- o.cursorlineopt ='both' -- to enable cursorline!
