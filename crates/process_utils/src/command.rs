5a290936158325b883395b7856378e7998a2b09a
Lisa White <legeana@liri.ie>
2025-02-13 02:57:33 +0000

    process_utils: support cmd!(["program", ...], ...) syntax

--- crates/process_utils/src/command.rs
+++ crates/process_utils/src/command.rs
@@ -130,7 +130,38 @@ macro_rules! cmd {
         ]
     };
 
+    // cmd!(["program"])
+    ([$program:expr]) => { cmd![@new $program] };
+    // cmd!(["program", ...])
+    ([$program:expr, $($tail:tt)*]) => {
+        cmd![
+            @push_down
+            ([$($tail)*])
+            ->
+            (cmd![@new $program])
+        ]
+    };
+    // cmd!(["program"], ...)
+    ([$program:expr], $($tail:tt)*) => {
+        cmd![
+            @push_down
+            ($($tail)*)
+            ->
+            (cmd![@new $program])
+        ]
+    };
+    // cmd!(["program", ...], ...)
+    ([$program:expr, $($arg:expr),* $(,)*], $($tail:tt)*) => {
+        cmd![
+            @push_down
+            ($($tail)*)
+            ->
+            (cmd![@new $program] $(.arg($arg))*)
+        ]
+    };
+    // cmd!("program")
     ($program:expr) => { cmd![@new $program] };
+    // cmd!("program", ...)
     ($program:expr, $($tail:tt)*) => {
         cmd![
             @push_down
@@ -369,6 +400,99 @@ mod tests {
     #[test]
     fn test_cmd_args_after_arg() {
         let cmd = cmd!("program", ["arg-1"], vec!["arg-2", "arg-3"]);
+
+        let std_cmd = cmd.finalise();
+        assert_eq!(std_cmd.get_program(), "program");
+        assert_eq!(
+            std_cmd.get_args().collect::<Vec<_>>(),
+            &["arg-1", "arg-2", "arg-3"],
+        );
+    }
+
+    #[test]
+    fn test_cmd_wrapped_program() {
+        let cmd = cmd!(["program"]);
+
+        let std_cmd = cmd.finalise();
+        assert_eq!(std_cmd.get_program(), "program");
+        assert_eq!(std_cmd.get_args().collect::<Vec<_>>(), &[] as &[&str]);
+    }
+
+    #[test]
+    fn test_cmd_wrapped_program_and_args() {
+        let cmd = cmd!(["program", "arg-1", "arg-2", "arg-3"]);
+
+        let std_cmd = cmd.finalise();
+        assert_eq!(std_cmd.get_program(), "program");
+        assert_eq!(
+            std_cmd.get_args().collect::<Vec<_>>(),
+            &["arg-1", "arg-2", "arg-3"],
+        );
+    }
+
+    #[test]
+    fn test_cmd_wrapped_program_and_args_trailing_wrapped_comma() {
+        let cmd = cmd!(["program", "arg-1", "arg-2", "arg-3",]);
+
+        let std_cmd = cmd.finalise();
+        assert_eq!(std_cmd.get_program(), "program");
+        assert_eq!(
+            std_cmd.get_args().collect::<Vec<_>>(),
+            &["arg-1", "arg-2", "arg-3"],
+        );
+    }
+
+    #[test]
+    fn test_cmd_wrapped_program_and_args_trailing_unwrapped_comma() {
+        let cmd = cmd!(["program", "arg-1", "arg-2", "arg-3"],);
+
+        let std_cmd = cmd.finalise();
+        assert_eq!(std_cmd.get_program(), "program");
+        assert_eq!(
+            std_cmd.get_args().collect::<Vec<_>>(),
+            &["arg-1", "arg-2", "arg-3"],
+        );
+    }
+
+    #[test]
+    fn test_cmd_wrapped_program_and_args_trailing_commas() {
+        let cmd = cmd!(["program", "arg-1", "arg-2", "arg-3",],);
+
+        let std_cmd = cmd.finalise();
+        assert_eq!(std_cmd.get_program(), "program");
+        assert_eq!(
+            std_cmd.get_args().collect::<Vec<_>>(),
+            &["arg-1", "arg-2", "arg-3"],
+        );
+    }
+
+    #[test]
+    fn test_cmd_wrapped_program_trailing_args() {
+        let cmd = cmd!(["program"], ["arg-1", "arg-2", "arg-3"]);
+
+        let std_cmd = cmd.finalise();
+        assert_eq!(std_cmd.get_program(), "program");
+        assert_eq!(
+            std_cmd.get_args().collect::<Vec<_>>(),
+            &["arg-1", "arg-2", "arg-3"],
+        );
+    }
+
+    #[test]
+    fn test_cmd_wrapped_program_and_args_trailing_args() {
+        let cmd = cmd!(["program", "arg-1", "arg-2"], ["arg-3"]);
+
+        let std_cmd = cmd.finalise();
+        assert_eq!(std_cmd.get_program(), "program");
+        assert_eq!(
+            std_cmd.get_args().collect::<Vec<_>>(),
+            &["arg-1", "arg-2", "arg-3"],
+        );
+    }
+
+    #[test]
+    fn test_cmd_wrapped_program_and_args_trailing_comma_trailing_args() {
+        let cmd = cmd!(["program", "arg-1", "arg-2",], ["arg-3"]);
 
         let std_cmd = cmd.finalise();
         assert_eq!(std_cmd.get_program(), "program");
