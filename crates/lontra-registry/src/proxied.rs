ac17bc32a2e4317cc11e8e0bb453c588cd2fd4fb
Lisa White <legeana@liri.ie>
2025-06-08 21:16:33 +0100

    lontra-registry: hide all macros-internal types in proxied::internal module

--- crates/lontra-registry/src/proxied.rs
+++ crates/lontra-registry/src/proxied.rs
@@ -1,39 +1,43 @@
 #![allow(dead_code, unused_imports)]
 
-pub(crate) use anyhow::Result;
-pub(crate) use sqlx::Database as SqlxDatabase;
-pub(crate) use sqlx::Decode as SqlxDecode;
-pub(crate) use sqlx::Encode as SqlxEncode;
-pub(crate) use sqlx::Type as SqlxType;
-pub(crate) use sqlx::encode::IsNull;
-pub(crate) use sqlx::error::BoxDynError;
-
-pub(crate) type BoxDynResult<T> = Result<T, BoxDynError>;
-
 pub(crate) trait Type {
     type Proxy;
 
     // Required.
-    fn into_proxy(self) -> Result<Self::Proxy>;
-    fn to_proxy(&self) -> Result<Self::Proxy>;
+    fn into_proxy(self) -> anyhow::Result<Self::Proxy>;
+    fn to_proxy(&self) -> anyhow::Result<Self::Proxy>;
 }
 
 pub(crate) trait SizedType: Type + Sized {
-    fn from_proxy(proxy: Self::Proxy) -> Result<Self>;
+    fn from_proxy(proxy: Self::Proxy) -> anyhow::Result<Self>;
+}
+
+// Internal types made public for macros only.
+// Do not use directly.
+pub(crate) mod internal {
+    pub(crate) use sqlx::Database as SqlxDatabase;
+    pub(crate) use sqlx::Decode as SqlxDecode;
+    pub(crate) use sqlx::Encode as SqlxEncode;
+    pub(crate) use sqlx::Type as SqlxType;
+    pub(crate) use sqlx::encode::IsNull;
+    pub(crate) use sqlx::error::BoxDynError;
+
+    pub(crate) type BoxDynResult<T> = Result<T, BoxDynError>;
+    pub(crate) type IsNullResult = BoxDynResult<IsNull>;
 }
 
 #[macro_export]
 macro_rules! sqlx_type_impl {
     ($type:ty) => {
-        impl<DB> $crate::proxied::SqlxType<DB> for $type
+        impl<DB> $crate::proxied::internal::SqlxType<DB> for $type
         where
-            DB: $crate::proxied::SqlxDatabase,
+            DB: $crate::proxied::internal::SqlxDatabase,
             Self: $crate::proxied::Type,
-            <Self as $crate::proxied::Type>::Proxy: $crate::proxied::SqlxType<DB>,
+            <Self as $crate::proxied::Type>::Proxy: $crate::proxied::internal::SqlxType<DB>,
         {
-            fn type_info() -> <DB as $crate::proxied::SqlxDatabase>::TypeInfo {
-                use $crate::proxied::SqlxType;
+            fn type_info() -> <DB as $crate::proxied::internal::SqlxDatabase>::TypeInfo {
                 use $crate::proxied::Type;
+                use $crate::proxied::internal::SqlxType;
 
                 <<Self as Type>::Proxy as SqlxType<DB>>::type_info()
             }
@@ -44,18 +48,18 @@ macro_rules! sqlx_type_impl {
 #[macro_export]
 macro_rules! sqlx_decode_impl {
     ($type:ty) => {
-        impl<'r, DB> $crate::proxied::SqlxDecode<'r, DB> for $type
+        impl<'r, DB> $crate::proxied::internal::SqlxDecode<'r, DB> for $type
         where
-            DB: $crate::proxied::SqlxDatabase,
+            DB: $crate::proxied::internal::SqlxDatabase,
             Self: $crate::proxied::SizedType,
-            <Self as $crate::proxied::Type>::Proxy: $crate::proxied::SqlxDecode<'r, DB>,
+            <Self as $crate::proxied::Type>::Proxy: $crate::proxied::internal::SqlxDecode<'r, DB>,
         {
             fn decode(
-                value: <DB as $crate::proxied::SqlxDatabase>::ValueRef<'r>,
-            ) -> BoxDynResult<Self> {
+                value: <DB as $crate::proxied::internal::SqlxDatabase>::ValueRef<'r>,
+            ) -> $crate::proxied::internal::BoxDynResult<Self> {
                 use $crate::proxied::SizedType;
-                use $crate::proxied::SqlxDecode;
                 use $crate::proxied::Type;
+                use $crate::proxied::internal::SqlxDecode;
 
                 let proxy = <<Self as Type>::Proxy as SqlxDecode<'r, DB>>::decode(value)?;
                 Ok(<Self as SizedType>::from_proxy(proxy)?)
@@ -67,26 +71,30 @@ macro_rules! sqlx_decode_impl {
 #[macro_export]
 macro_rules! sqlx_encode_impl {
     ($type:ty) => {
-        impl<'q, DB> $crate::proxied::SqlxEncode<'q, DB> for $type
+        impl<'q, DB> $crate::proxied::internal::SqlxEncode<'q, DB> for $type
         where
-            DB: $crate::proxied::SqlxDatabase,
+            DB: $crate::proxied::internal::SqlxDatabase,
             Self: $crate::proxied::Type,
-            <Self as $crate::proxied::Type>::Proxy: $crate::proxied::SqlxEncode<'q, DB>,
+            <Self as $crate::proxied::Type>::Proxy: $crate::proxied::internal::SqlxEncode<'q, DB>,
         {
             fn encode_by_ref(
                 &self,
-                buf: &mut <DB as $crate::proxied::SqlxDatabase>::ArgumentBuffer<'q>,
-            ) -> $crate::proxied::BoxDynResult<$crate::proxied::IsNull> {
+                buf: &mut <DB as $crate::proxied::internal::SqlxDatabase>::ArgumentBuffer<'q>,
+            ) -> $crate::proxied::internal::IsNullResult {
+                use $crate::proxied::Type as _;
+
                 let proxy = self.to_proxy()?;
                 proxy.encode_by_ref(buf)
             }
             fn encode(
                 self,
-                buf: &mut <DB as $crate::proxied::SqlxDatabase>::ArgumentBuffer<'q>,
-            ) -> $crate::proxied::BoxDynResult<$crate::proxied::IsNull>
+                buf: &mut <DB as $crate::proxied::internal::SqlxDatabase>::ArgumentBuffer<'q>,
+            ) -> $crate::proxied::internal::IsNullResult
             where
                 Self: Sized,
             {
+                use $crate::proxied::Type as _;
+
                 let proxy = self.into_proxy()?;
                 proxy.encode(buf)
             }
@@ -113,13 +121,14 @@ macro_rules! sqlx_impl {
 
 #[cfg(test)]
 mod tests {
+    use anyhow::Result;
     use sqlx::Sqlite;
 
     use super::*;
 
-    fn assert_type<T: SqlxType<Sqlite>>() {}
-    fn assert_decode<'r, T: SqlxDecode<'r, Sqlite>>() {}
-    fn assert_encode<'q, T: SqlxEncode<'q, Sqlite>>() {}
+    fn assert_type<T: sqlx::Type<Sqlite>>() {}
+    fn assert_decode<'r, T: sqlx::Decode<'r, Sqlite>>() {}
+    fn assert_encode<'q, T: sqlx::Encode<'q, Sqlite>>() {}
 
     // Tests for OwnedType.
     struct OwnedType;
@@ -141,7 +150,7 @@ mod tests {
         }
     }
 
-    sqlx_impl!(OwnedType);
+    crate::sqlx_impl!(OwnedType);
 
     #[test]
     fn test_owned() {
