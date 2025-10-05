cc880e136cd92627da1ca67bbac467db5d73ea13
Lisa White <legeana@liri.ie>
2025-10-05 19:29:27 +0100

    lontra-xtask: install rust target as dev dependencies

--- crates/lontra-xtask/src/dependencies.rs
+++ crates/lontra-xtask/src/dependencies.rs
@@ -4,10 +4,12 @@ use xshell::Shell;
 use xshell::cmd;
 
 const DEV_DEPS: &[&str] = &["cargo-xwin", "sqlx-cli"];
+const DEV_TARGETS: &[&str] = &["x86_64-pc-windows-gnu", "x86_64-unknown-linux-gnu"];
 
 pub fn install_dev() -> Result<()> {
     let sh = Shell::new()?;
     let cargo = sh.var_os("CARGO").context("failed to find CARGO")?;
     cmd!(sh, "{cargo} install --locked {DEV_DEPS...}").run()?;
+    cmd!(sh, "rustup target add {DEV_TARGETS...}").run()?;
     Ok(())
 }
