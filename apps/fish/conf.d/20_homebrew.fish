48678d0c342e2368db32bfcc1be99d86b4ccf48d
Lisa White <legeana@liri.ie>
2026-03-30 18:18:20 +0100

    fish: simplify homebrew path setup

---
+++ apps/fish/conf.d/20_homebrew.fish
@@ -0,0 +1,4 @@
+must-have-command brew
+
+eval (brew shellenv)
+fish_add_path "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
--- apps/fish/conf.d/20_linux_homebrew.fish
+++
@@ -1,2 +0,0 @@
-add_to_path /home/linuxbrew/.linuxbrew/{bin,sbin}
-add_to_manpath /home/linuxbrew/.linuxbrew/share/man
