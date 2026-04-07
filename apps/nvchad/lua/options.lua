e7ed85a7a535560808192a8700df105df0de9ac0
Lisa White <legeana@liri.ie>
2026-04-07 01:00:26 +0100

    nvchad: use require-dir for options and enable listchars and cursorline

--- apps/nvchad/lua/options.lua
+++ apps/nvchad/lua/options.lua
@@ -1,8 +1,2 @@
 require "nvchad.options"
-
--- add yours here!
-
-local o = vim.o
-o.list = true
-o.listchars = "tab:>.,trail:$,extends:#,nbsp:."
--- o.cursorlineopt ='both' -- to enable cursorline!
+require("lib.require-dir")("options")
---
+++ apps/nvchad/lua/options/cursorline.lua
@@ -0,0 +1,2 @@
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = 'both'
