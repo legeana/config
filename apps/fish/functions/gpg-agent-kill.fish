ff450816d6d6929f801ac9bd989cae77ce8262b1
Lisa White <legeana@liri.ie>
2026-05-03 10:34:41 +0200

    fish: remove gpg-agent functions

--- apps/fish/functions/gpg-agent-kill.fish
+++
@@ -1,5 +0,0 @@
-function gpg-agent-kill --description 'Kill running gpg-agent and cleanup variables'
-    gpgconf --kill gpg-agent
-    set -e GPG_TTY
-    set -e SSH_AUTH_SOCK
-end
--- apps/fish/functions/gpg-agent.fish
+++
@@ -1,6 +0,0 @@
-function gpg-agent --description 'Run gpg-agent'
-    set -xg GPG_TTY (tty)
-    gpg-connect-agent /bye
-    set -xg SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-    gpg-connect-agent updatestartuptty /bye
-end
